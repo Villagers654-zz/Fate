6870f5c60ae82b1dc36587e41ff112a33e040351
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-15 17:03:42 -0500

    stopped it from printing the errror unnessisarily

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -23,6 +23,7 @@ class ErrorHandler:
 # ~== Main ==~
 
 	async def on_command_error(self, ctx, error):
+		check = 0
 		if hasattr(ctx.command, 'on_error'):
 			return
 		ignored = (commands.CommandNotFound)
@@ -30,24 +31,30 @@ class ErrorHandler:
 		if isinstance(error, ignored):
 			return
 		elif isinstance(error, commands.DisabledCommand):
-			return await ctx.send(f'`{ctx.command}` has been disabled.')
+			await ctx.send(f'`{ctx.command}` has been disabled.')
+			check += 1
 		elif isinstance(error, commands.NoPrivateMessage):
 			try:
 				return await ctx.author.send(f'`{ctx.command}` can not be used in Private Messages.')
 			except:
 				pass
+			check += 1
 		elif isinstance(error, commands.BadArgument):
 			if ctx.command.qualified_name == 'tag list':
-				return await ctx.send('I could not find that member. Please try again.')
+				await ctx.send('I could not find that member. Please try again.')
+				check += 1
 		elif isinstance(error, commands.CommandOnCooldown):
-			return await ctx.message.add_reaction('⏳')
+			await ctx.message.add_reaction('⏳')
 		elif isinstance(error, commands.MissingRequiredArgument):
-			return await ctx.send(error)
+			await ctx.send(error)
+			check += 1
 		elif isinstance(error, commands.CheckFailure):
 			await ctx.message.add_reaction('⚠')
-		if isinstance(error, discord.errors.Forbidden):
+			check += 1
+		elif isinstance(error, discord.errors.Forbidden):
 			await ctx.send("I'm missing permissions")
-		else:
+			check += 1
+		if check == 0:
 			print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
 			traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 			await ctx.send(error)
