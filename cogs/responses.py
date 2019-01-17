5a179ce0504df5b3472e134e0d7e36308f53cf34
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-17 17:29:45 -0500

    error no longer occurs when responding to ghost messages

--- cogs/responses.py
+++ cogs/responses.py
@@ -36,35 +36,35 @@ class Toggles:
 # ~== Main ==~
 
 	async def on_message(self, m: discord.Message):
-		m.content = m.content.lower()
-		r = random.randint(1, 8)
-		if m.content.startswith("<@506735111543193601>"):
-			await m.channel.send(random.choice([
-				"Once apon a time in a land far away, there lived a little boy who pinged a bot, first came rape, then came aids",
-				"Don't ping me m8, it hurts you more than it hurts me",
-				"Oh look, another homosexual",
-				"FBI OPEN UP",
-				"pUrE wHiTe pRiVelIdgEd mALe^",
-				"Once apon a time in a land far away, there lived a little boy who pinged a bot, first came rape, then came aids",
-				"Do you need virtual daycare or something?",
-				"Shut your skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone friend zoned sylvester stallone sierra leone autozone professionally seen silver patrone head ass tf up.",
-				"Fuck off hitler",
-				"alright you pathetic lost child, use .help",
-				"and what might **you** want",
-				"No sir"]))
-		if m.content.startswith("sponsered by totherbot"):
-			await m.channel.send(random.choice([
-				"Sponsored by faggatron",
-				"Can someone ban that rotarded tother bot",
-				"*powered by tothers ego*"]))
-		if m.content.startswith("ew, fates here?"):
-			await m.channel.send(random.choice([
-				"ew, faggatron used my name",
-				"also can someone ban that rotarded tother bot",
-				"*powered by tothers ego*"]))
-		if "please, stop harrasing my bot" in m.content:
-			await m.channel.send('then please stop noticing me every time im pinged reeee')
 		if isinstance(m.guild, discord.Guild):
+			m.content = m.content.lower()
+			r = random.randint(1, 8)
+			if m.content.startswith("<@506735111543193601>"):
+				await m.channel.send(random.choice([
+					"Once apon a time in a land far away, there lived a little boy who pinged a bot, first came rape, then came aids",
+					"Don't ping me m8, it hurts you more than it hurts me",
+					"Oh look, another homosexual",
+					"FBI OPEN UP",
+					"pUrE wHiTe pRiVelIdgEd mALe^",
+					"Once apon a time in a land far away, there lived a little boy who pinged a bot, first came rape, then came aids",
+					"Do you need virtual daycare or something?",
+					"Shut your skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone friend zoned sylvester stallone sierra leone autozone professionally seen silver patrone head ass tf up.",
+					"Fuck off hitler",
+					"alright you pathetic lost child, use .help",
+					"and what might **you** want",
+					"No sir"]))
+			if m.content.startswith("sponsered by totherbot"):
+				await m.channel.send(random.choice([
+					"Sponsored by faggatron",
+					"Can someone ban that rotarded tother bot",
+					"*powered by tothers ego*"]))
+			if m.content.startswith("ew, fates here?"):
+				await m.channel.send(random.choice([
+					"ew, faggatron used my name",
+					"also can someone ban that rotarded tother bot",
+					"*powered by tothers ego*"]))
+			if "please, stop harrasing my bot" in m.content:
+				await m.channel.send('then please stop noticing me every time im pinged reeee')
 			if str(m.guild.id) not in self.responses:
 				self.responses[str(m.guild.id)] = 'enabled'
 				with open("./data/userdata/config/toggles.json", "w") as outfile:
