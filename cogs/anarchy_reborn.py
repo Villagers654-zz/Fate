f1acdc89fd0d07281c4355597cf691cb7af2a9b7
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-16 23:51:57 -0500

    temp solution for ar

---
+++ cogs/anarchy_reborn.py
@@ -0,0 +1,15 @@
+import discord
+import asyncio
+
+class Owner:
+	def __init__(self, bot):
+		self.bot = bot
+
+	async def on_message(self, m:discord.Message):
+		if m.channel.id == 533817312634208257:
+			if m.id is not 534039043483369482:
+				await asyncio.sleep(3600)
+				await m.delete()
+
+def setup(bot):
+	bot.add_cog(Owner(bot))
