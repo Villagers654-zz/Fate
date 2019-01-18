3f77d3895507318520d9517681095f2cebfbc4b8
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-17 20:49:22 -0500

    added someth for meh

--- cogs/archive.py
+++ cogs/archive.py
@@ -37,6 +37,24 @@ class customclass:
 				await ctx.send(file=discord.File(path))
 				os.system(f'rm data/{ctx.channel.name}.txt')
 
+	@commands.command()
+	@commands.check(luck)
+	@commands.cooldown(1, 120, commands.BucketType.channel)
+	async def sss(self, ctx, amount:int):
+		c = 0
+		if c == 0:
+			async with ctx.typing():
+				log = ""
+				async for msg in ctx.channel.history(limit=amount):
+					log += f"""{msg.author.name}: {msg.content}\n"""
+				ctx.channel.name = ctx.channel.name.replace(" ", "-")
+				f = open(f'/home/luck/FateZero/data/{ctx.channel.name}.txt', 'w')
+				f.write(log)
+				f.close()
+				path = os.getcwd() + f"/data/{ctx.channel.name}.txt"
+				await ctx.send(file=discord.File(path))
+				os.system(f'rm data/{ctx.channel.name}.txt')
+
 # ~== Pings ==~
 
 def setup(bot):
