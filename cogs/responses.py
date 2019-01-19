e4bc381d227e0bf84cec8af79e57037456258e2a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-19 02:31:49 -0500

    majestic cleanup

--- cogs/responses.py
+++ cogs/responses.py
@@ -4,7 +4,7 @@ import discord
 import random
 import json
 
-class Toggles:
+class Responses:
 	def __init__(self, bot):
 		self.bot = bot
 		self.responses = {}
@@ -18,7 +18,7 @@ class Toggles:
 		return ctx.author.id == 264838866480005122
 
 	@commands.command()
-	@commands.check(luck)
+	@commands.has_permissions(manage_guild=True)
 	async def disableresponses(self, ctx):
 		self.responses[str(ctx.guild.id)] = "disabled"
 		await ctx.send("Disabled responses")
@@ -26,7 +26,7 @@ class Toggles:
 			json.dump({"responses": self.responses}, outfile, ensure_ascii=False)
 
 	@commands.command()
-	@commands.check(luck)
+	@commands.has_permissions(manage_guild=True)
 	async def enableresponses(self, ctx):
 		self.responses[str(ctx.guild.id)] = "enabled"
 		await ctx.send("Enabled responses")
@@ -36,6 +36,8 @@ class Toggles:
 # ~== Main ==~
 
 	async def on_message(self, m: discord.Message):
+		# core responses
+		r = random.randint(1, 4)
 		if m.content.startswith("<@506735111543193601>"):
 			check = 0
 			m.content = m.content.replace("<@506735111543193601>  ", "")
@@ -51,8 +53,7 @@ class Toggles:
 					f = open("./data/misc/responses.txt", "a")
 					f.write("\n" + m.content)
 					f.close()
-			r = random.randint(1, 4)
-			if r >= 3:
+			if r > 2:
 				msg = random.choice(open("./data/misc/responses.txt", "r").readlines())
 			else:
 				msg = random.choice([
@@ -69,51 +70,38 @@ class Toggles:
 					"and what might **you** want",
 					"No sir"])
 			await m.channel.send(msg)
-		if isinstance(m.guild, discord.Guild):
-			m.content = m.content.lower()
-			r = random.randint(1, 8)
-			if m.content.startswith("sponsered by totherbot"):
-				await m.channel.send(random.choice([
-					"Sponsored by faggatron",
-					"Can someone ban that rotarded tother bot",
-					"*powered by tothers ego*"]))
-			if m.content.startswith("ew, fates here?"):
-				await m.channel.send(random.choice([
-					"ew, faggatron used my name",
-					"also can someone ban that rotarded tother bot",
-					"*powered by tothers ego*"]))
-			if "please, stop harrasing my bot" in m.content:
-				await m.channel.send('then please stop noticing me every time im pinged reeee')
-			if str(m.guild.id) not in self.responses:
-				self.responses[str(m.guild.id)] = 'enabled'
-				with open("./data/userdata/config/toggles.json", "w") as outfile:
-					json.dump({"responses": self.responses}, outfile, ensure_ascii=False)
-			if self.responses[str(m.guild.id)] == 'enabled':
-				if r >= 7:
-					if m.content.startswith("hello"):
-						await m.channel.send(random.choice(["Hello", "Hello :3", "Suh", "Suh :3", "Wazzuh", "Despacito :]"]))
-					if m.content.startswith("gm"):
-						await m.channel.send(random.choice(["Gm", "Gm :3", "Morning", "Morning :3", "Welcome to heaven"]))
-					if m.content.startswith("gn"):
-						await m.channel.send(random.choice(["Gn", "Gn :3", "Night", "Nighty"]))
-					if m.content.startswith("ree"):
-						await m.channel.send(random.choice([
-							"*depression strikes again*", "*pole-man strikes again*",
-							"Would you like an espresso for your depresso",
-							"You're not you when you're hungry",
-							"*crippling depression*",
-							"Breakdown sponsored by Samsung",
-							"No espresso for you",
-							"Sucks to be you m8",
-							"Ripperoni"]))
-					if m.content.startswith("kys"):
-						await m.channel.send(random.choice([
-							"*nazi vegan feminism rally starts*",
-							"Sorry hitler, Germany's not here", "NoT iN mY cHriSTiAn sErVeR..\nDo it in threadys",
-							"tfw you see faggots that should kill themselves telling other people to kill themselves",
-							"Shut your skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone friend zoned sylvester stallone sierra leone autozone professionally seen silver patrone head ass tf up.",
-							"Well aren't you just a fun filled little lollipop tripple dipped in psycho",
-							"Woah, calm down satan"]))
+		m.content = m.content.lower()
+		#toggleable responses
+		if str(m.guild.id) not in self.responses:
+			self.responses[str(m.guild.id)] = 'enabled'
+			with open("./data/userdata/config/toggles.json", "w") as outfile:
+				json.dump({"responses": self.responses}, outfile, ensure_ascii=False)
+		if self.responses[str(m.guild.id)] == 'enabled':
+			if r > 2:
+				if m.content.startswith("hello"):
+					await m.channel.send(random.choice(["Hello", "Hello :3", "Suh", "Suh :3", "Wazzuh", "Despacito :]"]))
+				if m.content.startswith("gm"):
+					await m.channel.send(random.choice(["Gm", "Gm :3", "Morning", "Morning :3", "Welcome to heaven"]))
+				if m.content.startswith("gn"):
+					await m.channel.send(random.choice(["Gn", "Gn :3", "Night", "Nighty"]))
+				if m.content.startswith("ree"):
+					await m.channel.send(random.choice([
+						"*depression strikes again*", "*pole-man strikes again*",
+						"Would you like an espresso for your depresso",
+						"You're not you when you're hungry",
+						"*crippling depression*",
+						"Breakdown sponsored by Samsung",
+						"No espresso for you",
+						"Sucks to be you m8",
+						"Ripperoni"]))
+				if m.content.startswith("kys"):
+					await m.channel.send(random.choice([
+						"*nazi vegan feminism rally starts*",
+						"Sorry hitler, Germany's not here", "NoT iN mY cHriSTiAn sErVeR..\nDo it in threadys",
+						"tfw you see faggots that should kill themselves telling other people to kill themselves",
+						"Shut your skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone friend zoned sylvester stallone sierra leone autozone professionally seen silver patrone head ass tf up.",
+						"Well aren't you just a fun filled little lollipop tripple dipped in psycho",
+						"Woah, calm down satan"]))
 
 def setup(bot):
-	bot.add_cog(Toggles(bot))
+	bot.add_cog(Responses(bot))
