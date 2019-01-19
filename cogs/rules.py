5ead1da4779b7dc959425dc2b2f1c7f7bb5fb89f
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-19 02:32:06 -0500

    added per-guild rules

---
+++ cogs/rules.py
@@ -0,0 +1,39 @@
+from discord.ext import commands
+from os.path import isfile
+import discord
+import json
+
+class Mod:
+	def __init__(self, bot):
+		self.bot = bot
+		self.rules = {}
+		if isfile("./data/userdata/rules.json"):
+			with open("./data/userdata/rules.json", "r") as infile:
+				dat = json.load(infile)
+				if "rules" in dat:
+					self.rules = dat["rules"]
+
+	@commands.group(name="rules")
+	async def _rules(self, ctx):
+		if ctx.invoked_subcommand is None:
+			if str(ctx.guild.id) not in self.rules:
+				await ctx.send("This server doesnt have any rules set, try using .rules help")
+			else:
+				e = discord.Embed(color=0xff0000)
+				e.description = self.rules[str(ctx.guild.id)]
+				await ctx.send(embed=e)
+
+	@_rules.command(name="help")
+	async def _help(self, ctx):
+		await ctx.send(";-;")
+
+	@_rules.command(name="set")
+	@commands.has_permissions(manage_guild=True)
+	async def _set(self, ctx, *, rules):
+		self.rules[str(ctx.guild.id)] = rules
+		await ctx.send("Successfully set the rules 👍")
+		with open("./data/userdata/rules.json", "w") as outfile:
+			json.dump({"rules": self.rules}, outfile, ensure_ascii=False)
+
+def setup(bot):
+	bot.add_cog(Mod(bot))
