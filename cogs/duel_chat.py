dc17789442fcaa686fd4870a222cc1e7bf2694ff
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-19 02:27:04 -0500

    switched guilds

--- cogs/duel_chat.py
+++ cogs/duel_chat.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 
-class AR:
+class Owner:
 	def __init__(self, bot):
 		self.bot = bot
 
@@ -14,7 +14,7 @@ class AR:
 				if msg.author.id == self.bot.user.id:
 					pass
 				else:
-					ar = self.bot.get_guild(533796879410921482)
+					ar = self.bot.get_guild(530973099886379008)
 					for channel in ar.channels:
 						if channel.name == msg.channel.name:
 							async for m in channel.history(limit=1):
@@ -24,14 +24,11 @@ class AR:
 									await channel.send(f"{msg.content}")
 									await asyncio.sleep(0.5)
 									await msg.delete()
-			if msg.guild.name == "Anarchy Reborn":
-				if msg.channel.name == "council-chat":
-					pass
-				else:
-					polis = self.bot.get_guild(534949405824909323)
-					for channel in polis.channels:
-						if channel.name == msg.channel.name:
-								await channel.send(f"**{msg.author.name}:** {msg.content}")
+			if msg.guild.name == "[Avapzian Regime]":
+				polis = self.bot.get_guild(534949405824909323)
+				for channel in polis.channels:
+					if channel.name == msg.channel.name:
+						await channel.send(f"**{msg.author.name}:** {msg.content}")
 
 def setup(bot):
-	bot.add_cog(AR(bot))
+	bot.add_cog(Owner(bot))
