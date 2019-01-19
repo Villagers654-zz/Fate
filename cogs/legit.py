22921fdce39ac53e61183916ca96205a6d41e403
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-19 16:11:46 -0500

    disabled stuff for legits bot

--- cogs/legit.py
+++ cogs/legit.py
@@ -16,21 +16,21 @@ class Legit:
 
 # ~== Test ==~
 
-	async def on_message(self, m: discord.Message):
-		if m.channel.id == 529705827716825098:
-			if m.author.id == 529287770233896961:
-				if len(m.embeds) >= 1:
-					await asyncio.sleep(15)
-					try:
-						await m.delete()
-					except commands.MissingPermissions:
-						c = self.bot.get_channel(514213558549217330)
-						await c.send("I'm missing permissions in the livechat")
-					except discord.errors.NotFound:
-						pass
-					except Exception as e:
-						c = self.bot.get_channel(514213558549217330)
-						await c.send(f"Livechat error: {e}")
+#	async def on_message(self, m: discord.Message):
+#		if m.channel.id == 529705827716825098:
+#			if m.author.id == 529287770233896961:
+#				if len(m.embeds) >= 1:
+#					await asyncio.sleep(15)
+#					try:
+#						await m.delete()
+#					except commands.MissingPermissions:
+#						c = self.bot.get_channel(514213558549217330)
+#						await c.send("I'm missing permissions in the livechat")
+#					except discord.errors.NotFound:
+#						pass
+#					except Exception as e:
+#						c = self.bot.get_channel(514213558549217330)
+#						await c.send(f"Livechat error: {e}")
 
 	@commands.command()
 	@commands.check(luck)
