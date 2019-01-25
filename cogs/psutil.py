20e882b47e266c6286c2096044c0fd7043fc521e
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-24 22:07:18 -0500

    usages no longer frozen

--- cogs/psutil.py
+++ cogs/psutil.py
@@ -1,52 +1,52 @@
 from cogs.utils import psutil as p
 from discord.ext import commands
 import discord
+import psutil
+import os
 
-class Owner:
+class Psutil:
 	def __init__(self, bot):
 		self.bot = bot
-
-	def luck(ctx):
-		return ctx.message.author.id == 264838866480005122
 
 	@commands.command()
 	@commands.cooldown(1, 5, commands.BucketType.user)
 	async def devstats(self, ctx):
 		luck = self.bot.get_user(264838866480005122)
+		f = psutil.Process(os.getpid())
 		e = discord.Embed()
 		e.set_author(name='| Memory | ', icon_url=luck.avatar_url)
 		e.set_thumbnail(url="https://cdn.discordapp.com/attachments/514213558549217330/514345278669848597/8yx98C.gif")
-		e.description = f'__**Storage**__: [{p.bytes2human(p.storageused)}/{p.bytes2human(p.storagetotal)}]\n' \
-			f'__**RAM**__: [{p.bytes2human(p.ramused)}/{p.bytes2human(p.ramtotal)}] ({p.rampercent}%)\n' \
-			f'__**CPU**__: **Global**: {p.cpu}% **Bot**: {p.botcpu}%\n' \
-			f'__**CPU Frequency**__: [{p.freq}/{p.freqmax}]\n' \
+		e.description = f"__**Storage**__: [{p.bytes2human(psutil.disk_usage('/').used)}/{p.bytes2human(psutil.disk_usage('/').total)}]\n" \
+			f"__**RAM**__: [{p.bytes2human(psutil.virtual_memory().used)}/{p.bytes2human(psutil.virtual_memory().total)}] ({p.rampercent}%)\n" \
+			f"__**CPU**__: **Global**: {psutil.cpu_percent(interval=1)}% **Bot**: {f.cpu_percent(interval=1)}%\n" \
+			f'__**CPU Frequency**__: [{p.bytes2human(psutil.cpu_freq())}/{p.bytes2human(psutil.cpu_freq().max)}]\n' \
 			f'__**battery**__: {p.batterypercent}% {p.ischarging}'
 		e.set_footer(text=f'{p.percpu}')
 		await ctx.send(embed=e)
 
 	@commands.command()
 	async def freq(self, ctx):
-		await ctx.send(p.freq)
+		await ctx.send(p.bytes2human(psutil.cpu_freq()))
 
 	@commands.command()
 	async def pids(self, ctx):
-		await ctx.send(p.pids)
+		await ctx.send(psutil.pids())
 
 	@commands.command()
 	async def temp(self, ctx):
-		await ctx.send(p.temp)
+		await ctx.send(psutil.sensors_temperatures(fahrenheit=True))
 
 	@commands.command()
 	async def net(self, ctx):
-		await ctx.send(p.net)
+		await ctx.send(psutil.net_connections())
 
 	@commands.command(name='ram', aliases=['wam'])
 	async def ram(self, ctx):
-		await ctx.send(f'[{p.ramused}, {p.bytes2human(p.ramtotal)}]')
+		await ctx.send(f'[{p.bytes2human(psutil.virtual_memory())}, {p.bytes2human(psutil.virtual_memory().total)}]')
 
 	@commands.command()
 	async def cpu(self, ctx):
-		await ctx.send(f"{p.cpu}")
+		await ctx.send(f"{psutil.cpu_percent(interval=1)}")
 
 def setup(bot):
-	bot.add_cog(Owner(bot))
+	bot.add_cog(Psutil(bot))
