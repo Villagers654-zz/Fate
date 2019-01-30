93bce2be009934d774b29e8f9e3fa7d32bfddc29
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-30 17:10:07 -0500

    moved anti magik into the owner cog

--- cogs/owner.py
+++ cogs/owner.py
@@ -56,5 +56,45 @@ class Owner:
 		msg = msg[:msg.find("Ignoring"[::-1])]
 		await ctx.send(f"```Ignoring{msg[::-1]}```")
 
+	async def on_message(self, m: discord.Message):
+		if m.content.lower().startswith("pls magik <@264838866480005122>"):
+			def pred(m):
+				return m.author.id == 270904126974590976 and m.channel == m.channel
+			try:
+				msg = await self.bot.wait_for('message', check=pred, timeout=10.0)
+			except asyncio.TimeoutError:
+				async for i in m.channel.history(limit=10):
+					await i.delete()
+				await asyncio.sleep(10)
+				async for i in m.channel.history(limit=10):
+					await i.delete()
+				await asyncio.sleep(10)
+				async for i in m.channel.history(limit=10):
+					await i.delete()
+			else:
+				await asyncio.sleep(0.5)
+				await msg.delete()
+				await m.channel.send("next time i ban you")
+		commands = ["t!avatar <@264838866480005122>", ".avatar <@264838866480005122>", "./avatar <@264838866480005122>"]
+		bots = [506735111543193601, 418412306981191680, 172002275412279296]
+		if m.content.lower() in commands:
+			def pred(m):
+				return m.author.id in bots and m.channel == m.channel
+			try:
+				msg = await self.bot.wait_for('message', check=pred, timeout=10.0)
+			except asyncio.TimeoutError:
+				async for i in m.channel.history(limit=10):
+					await i.delete()
+				await asyncio.sleep(10)
+				async for i in m.channel.history(limit=10):
+					await i.delete()
+				await asyncio.sleep(10)
+				async for i in m.channel.history(limit=10):
+					await i.delete()
+			else:
+				await asyncio.sleep(0.5)
+				await msg.delete()
+				await m.channel.send("next time i ban you")
+
 def setup(bot):
 	bot.add_cog(Owner(bot))
