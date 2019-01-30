ebb7022b0f75d3d25c7b355e354ebb7b106f880a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-01-30 17:09:36 -0500

    added a stats message for arkadia

--- cogs/profiles.py
+++ cogs/profiles.py
@@ -445,9 +445,67 @@ class Profiles:
 			except Exception as e:
 				await self.bot.get_channel(534608853300412416).send(f"```{traceback.format_exc()}```{e}")
 
+	async def arkadia_stats(self):
+		while True:
+			try:
+				channel = self.bot.get_channel(540086847842549770)
+				channels = 0
+				for server in self.bot.guilds:
+					for c in server.channels:
+						channels += 1
+				e = discord.Embed(title="", color=0x4A0E50)
+				e.description = "💎 Arkadia 💎"
+				leaderboard = ""
+				rank = 1
+				for user_id, xp in (sorted(self.guilds_data[str(channel.guild.id)].items(), key=lambda kv: kv[1], reverse=True))[:8]:
+					name = "INVALID-USER"
+					user = self.bot.get_user(int(user_id))
+					if isinstance(user, discord.User):
+						name = user.name
+					level = str(xp / 750)
+					level = level[:level.find(".")]
+					leaderboard += "‎**‎#{}.** ‎`‎{}`: ‎{} | {}\n".format(rank, name, level, xp)
+					rank += 1
+				f = psutil.Process(os.getpid())
+				try:
+					cpufreqcurrent = p.bytes2human(psutil.cpu_freq().current)
+				except:
+					cpufreqcurrent = "unavailable"
+				try:
+					cpufreqmax = p.bytes2human(psutil.cpu_freq().max)
+				except:
+					cpufreqmax = "unavailable"
+				e.set_thumbnail(url=channel.guild.icon_url)
+				e.set_author(name=f'~~~====🥂🍸🍷Stats🍷🍸🥂====~~~')
+				e.add_field(name="◈ Discord ◈", value=f'__**Owner**__: Luck\n__**Members**__: {channel.guild.member_count}', inline=False)
+				e.add_field(name="Leaderboard", value=leaderboard, inline=False)
+				e.add_field(
+					name="◈ Memory ◈",
+					value=f"__**Storage**__: [{p.bytes2human(psutil.disk_usage('/').used)}/{p.bytes2human(psutil.disk_usage('/').total)}]\n"
+					f"__**RAM**__: **Global**: {p.bytes2human(psutil.virtual_memory().used)} **Bot**: {p.bytes2human(f.memory_full_info().rss)}\n"
+					f"__**CPU**__: **Global**: {psutil.cpu_percent(interval=1)}% **Bot**: {f.cpu_percent(interval=1)}%\n"
+					f"__**CPU Per Core**__: {[round(i) for i in psutil.cpu_percent(interval=1, percpu=True)]}\n"
+					f"__**CPU Frequency**__: [{cpufreqcurrent}/{cpufreqmax}]")
+				e.add_field(name=f"◈ {self.bot.user.name} ◈", value=
+				f"**Commands:** {len(self.bot.commands)}\n"
+				f"**Modules:** {len(self.bot.extensions)}\n"
+				f"**Servers:** {len(list(self.bot.guilds))}\n"
+				f"**Channels:** {channels}\n"
+				f"**Users:** {len(list(self.bot.users))}\n")
+				fmt = "%m-%d-%Y %I:%M%p"
+				time = datetime.datetime.now()
+				time = time.strftime(fmt)
+				e.set_footer(text=f'Updated: {time}')
+				message = await channel.get_message(540096913995726848)
+				await message.edit(embed=e)
+				await asyncio.sleep(600)
+			except Exception as e:
+				await self.bot.get_channel(534608853300412416).send(f"```{traceback.format_exc()}```{e}")
+
 	async def on_ready(self):
 		await asyncio.sleep(0.5)
 		self.bot.loop.create_task(self.stats())
+		self.bot.loop.create_task(self.arkadia_stats())
 
 	async def on_message(self, m:discord.Message):
 		if isinstance(m.guild, discord.Guild):
