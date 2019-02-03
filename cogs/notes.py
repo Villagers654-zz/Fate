8aa102babce6fe4bbcba78f8dfeb031186d8e3ae
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-03 05:43:30 -0500

    typing stops after the message is sent

--- cogs/notes.py
+++ cogs/notes.py
@@ -21,31 +21,32 @@ class Notepad:
 
     @commands.command()
     async def note(self, ctx, *, arg: commands.clean_content = ""):
-        async with ctx.typing():
-            if len(arg) > 0:
+        if len(arg) > 0:
+            async with ctx.typing():
                 self.notes[str(ctx.author.id)] = arg
                 fmt = "%m-%d-%Y %I:%M%p"
                 created = datetime.datetime.now()
                 self.timestamp[str(ctx.author.id)] = created.strftime(fmt)
-                path = os.getcwd() + "/data/images/reactions/notes/" + random.choice(os.listdir(os.getcwd() + "/data/images/reactions/notes/"))
+                path = os.getcwd() + "/data/images/reactions/notes/" + random.choice(
+                    os.listdir(os.getcwd() + "/data/images/reactions/notes/"))
                 e = discord.Embed(color=0xFFC923)
                 e.set_author(name='Noted..', icon_url=ctx.author.avatar_url)
                 e.set_image(url="attachment://" + os.path.basename(path))
                 await ctx.send(file=discord.File(path, filename=os.path.basename(path)), embed=e, delete_after=10)
-                await asyncio.sleep(10)
-                await ctx.message.delete()
-                with open("./data/userdata/notes.json", "w") as outfile:
-                    json.dump({"notes": self.notes, "timestamp": self.timestamp}, outfile, ensure_ascii=False)
+            await asyncio.sleep(10)
+            await ctx.message.delete()
+            with open("./data/userdata/notes.json", "w") as outfile:
+                return json.dump({"notes": self.notes, "timestamp": self.timestamp}, outfile, ensure_ascii=False)
+        else:
+            if str(ctx.author.id) in self.notes:
+                e = discord.Embed(color=0xFFC923)
+                e.title = "~~===🥂🍸🍷Note🍷🍸🥂===~~"
+                e.set_thumbnail(url=ctx.author.avatar_url)
+                e.description = self.notes[str(ctx.author.id)]
+                e.set_footer(text=self.timestamp[str(ctx.author.id)])
+                await ctx.send(embed=e)
             else:
-                if str(ctx.author.id) in self.notes:
-                    e = discord.Embed(color=0xFFC923)
-                    e.title = "~~===🥂🍸🍷Note🍷🍸🥂===~~"
-                    e.set_thumbnail(url=ctx.author.avatar_url)
-                    e.description = self.notes[str(ctx.author.id)]
-                    e.set_footer(text=self.timestamp[str(ctx.author.id)])
-                    await ctx.send(embed=e)
-                else:
-                    await ctx.send("no data")
+                await ctx.send("no data")
 
 def setup(bot: commands.Bot):
     bot.add_cog(Notepad(bot))
