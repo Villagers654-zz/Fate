339c53257cdc4cf1efa1c1414d6dfcd88a59abce
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-06 06:12:31 -0500

    edited an error handler

--- cogs/save.py
+++ cogs/save.py
@@ -189,7 +189,7 @@ class ServerSave:
 			for role in ctx.guild.roles[:]:
 				try:
 					if role.name not in [x["name"] for x in g["roles"]]:
-						if role.name == "Fate [Zero]":
+						if "Fate" in role.name:
 							pass
 						else:
 							await role.delete(reason="Loading saved server")
