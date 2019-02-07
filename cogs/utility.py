e2fb412717d151af1817956917a44883d276d2fc
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-07 05:43:33 -0500

    moved emoji commands

--- cogs/utility.py
+++ cogs/utility.py
@@ -59,58 +59,6 @@ class Utility:
 		e.add_field(name="◈ Created ◈", value=created.strftime(fmt), inline=False)
 		await ctx.send(embed=e)
 
-	@commands.command(name="addemoji", aliases=["emote", "addemote"])
-	@commands.has_permissions(manage_emojis=True)
-	async def _addemoji(self, ctx, *, name=None):
-		try:
-			if len(ctx.message.attachments) > 1:
-				for attachment in ctx.message.attachments:
-					name = attachment[:str(attachment.filename).lower().find(".")].replace(" ", "")[:32]
-					await ctx.guild.create_custom_emoji(name=name, image=requests.get(attachment.url).content,
-					                                    reason=ctx.author.name)
-					await ctx.send(f"successfully added `{name}` to emotes")
-			else:
-				if name is None:
-					for attachment in ctx.message.attachments:
-						name = attachment[:str(attachment.filename).lower().find(".")].replace(" ", "")[:32]
-						await ctx.guild.create_custom_emoji(name=name, image=requests.get(attachment.url).content,
-						                                    reason=ctx.author.name)
-						await ctx.send(f"Successfully added `{name}` to emotes")
-				else:
-					for attachment in ctx.message.attachments:
-						name = name[:32].replace(" ", "")
-						await ctx.guild.create_custom_emoji(name=name, image=requests.get(attachment.url).content,
-						                                    reason=ctx.author.name)
-						await ctx.send(f"Successfully added `{name}` to emotes")
-		except Exception as HTTPException:
-			for attachment in ctx.message.attachments:
-				attachment = attachment
-			e = discord.Embed(color=colors.fate())
-			e.set_author(name=f"File cannot be larger than 256 kb", icon_url=attachment.proxy_url)
-			e.set_thumbnail(url=ctx.author.avatar_url)
-			e.description = f"Try using [TinyPNG](https://tinypng.com/) to reduce the size"
-			await ctx.send(embed=e)
-
-	@commands.command(name="delemoji", aliases=["delemote"])
-	@commands.has_permissions(manage_emojis=True)
-	async def _delemoji(self, ctx, *, name):
-		check = 0
-		for emote in ctx.guild.emojis:
-			if name.lower() == emote.name.lower():
-				await emote.delete(reason=ctx.author.name)
-				await ctx.send(f"Deleted emote `{emote.name}`")
-				check = 1
-				break
-		if check == 0:
-			for emote in ctx.guild.emojis:
-				if name.lower() in emote.name.lower():
-					await emote.delete(reason=ctx.author.name)
-					await ctx.send(f"Deleted emote `{emote.name}`")
-					check = 1
-					break
-		if check == 0:
-			await ctx.send("I couldnt find that emote")
-
 	@commands.command(name='makepoll', aliases=['mp'])
 	@commands.cooldown(1, 5, commands.BucketType.channel)
 	@commands.has_permissions(manage_messages=True)
