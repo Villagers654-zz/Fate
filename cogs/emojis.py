4b892f5e7c966cb398c97d717c83ec10cf3431a7
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-08 12:03:42 -0500

    added _fromemoji to add pre-existing emotes

--- cogs/emojis.py
+++ cogs/emojis.py
@@ -9,35 +9,37 @@ class Emojis:
 
 	@commands.command(name="addemoji", aliases=["emote", "addemote"])
 	@commands.has_permissions(manage_emojis=True)
-	async def _addemoji(self, ctx, *, name=None):
+	async def _addemoji(self, ctx, name=None):
 		try:
 			chars = list("abcdefghijklmnopqrstuvwxyz")
 			if len(ctx.message.attachments) > 1:
 				for attachment in ctx.message.attachments:
 					clean = ""
-					for i in list(attachment.name):
-						if i in chars:
+					name = str(attachment.filename)
+					name = name[:name.find(".")]
+					for i in list(name):
+						if i.lower() in chars:
 							clean += i
-					name = attachment[:str(attachment.filename).lower().find(".")].replace(" ", "")[:32]
 					await ctx.guild.create_custom_emoji(name=clean, image=requests.get(attachment.url).content, reason=ctx.author.name)
-					await ctx.send(f"successfully added `{name}` to emotes")
+					await ctx.send(f"successfully added `{clean}` to emotes")
 			else:
 				if name is None:
 					for attachment in ctx.message.attachments:
 						clean = ""
-						for i in list(attachment.name):
-							if i in chars:
+						name = str(attachment.filename)
+						name = name[:name.find(".")]
+						for i in list(name):
+							if i.lower() in chars:
 								clean += i
-						name = attachment[:str(attachment.filename).lower().find(".")].replace(" ", "")[:32]
 						await ctx.guild.create_custom_emoji(name=clean, image=requests.get(attachment.url).content, reason=ctx.author.name)
-						await ctx.send(f"Successfully added `{name}` to emotes")
+						await ctx.send(f"Successfully added `{clean}` to emotes")
 				else:
 					for attachment in ctx.message.attachments:
 						name = name[:32].replace(" ", "")
 						await ctx.guild.create_custom_emoji(name=name, image=requests.get(attachment.url).content, reason=ctx.author.name)
 						await ctx.send(f"Successfully added `{name}` to emotes")
 		except Exception as HTTPException:
-			if "256kb" in HTTPException:
+			if "256kb" in str(HTTPException):
 				for attachment in ctx.message.attachments:
 					attachment = attachment
 				e = discord.Embed(color=colors.fate())
@@ -45,7 +47,12 @@ class Emojis:
 				e.set_thumbnail(url=ctx.author.avatar_url)
 				e.description = f"Try using [TinyPNG](https://tinypng.com/) to reduce the size"
 				return await ctx.send(embed=e)
-			await ctx.send(HTTPException[:2000])
+			await ctx.send(str(HTTPException)[:2000])
+
+	@commands.command(name="fromemoji", aliases=["fromemote"])
+	async def _fromemoji(self, ctx, emoji: discord.PartialEmoji):
+		await ctx.guild.create_custom_emoji(name=emoji.name, image=requests.get(emoji.url).content, reason=ctx.author.name)
+		await ctx.send(f"Successfully added `{emoji.name}` to emotes")
 
 	@commands.command(name="delemoji", aliases=["delemote"])
 	@commands.has_permissions(manage_emojis=True)
