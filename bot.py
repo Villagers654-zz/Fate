8828bf7819d373ed4f3bc4e801abb9f0e1a17564
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-11 23:47:02 -0500

    changed the channel the bot sends startup errors to

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ async def on_ready():
 	created = datetime.datetime.now()
 	cprint(created.strftime(fmt), 'yellow')
 	if error is not False:
-		await bot.get_channel(534608853300412416).send(f"```{error}```")
+		await bot.get_channel(514214974868946964).send(f"```{error}```")
 	bot.loop.create_task(status_task())
 	m, s = divmod(time.time() - bot.START_TIME, 60)
 	h, m = divmod(m, 60)
