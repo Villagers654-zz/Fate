1db48e44abe63d769281800962d4db8c2e344e20
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-15 01:51:20 -0500

    rewrite

--- cogs/anti_purge.py
+++ cogs/anti_purge.py
@@ -23,21 +23,18 @@ class AntiPurge:
 	async def _anti_purge(self, ctx):
 		if ctx.invoked_subcommand is None:
 			guild_id = str(ctx.guild.id)
-			if guild_id not in self.toggle:
-				self.toggle[guild_id] = "disabled"
+			toggle = "disabled"
+			if guild_id in self.toggle:
+				toggle = "enabled"
 			await ctx.send("**Anti Purge Instructions:**\n"
 			               ".anti_purge enable\n"
 			               ".anti_purge disable\n"
-			               f"**Current Status:** {self.toggle[guild_id]}\n")
+			               f"**Current Status:** {toggle}\n")
 
 	@_anti_purge.command(name="enable")
 	@commands.has_permissions(administrator=True)
 	async def _enable(self, ctx):
 		guild_id = str(ctx.guild.id)
-		if guild_id not in self.toggle:
-			self.toggle[guild_id] = "enabled"
-			self.save()
-			return await ctx.message.add_reaction("👍")
 		self.toggle[guild_id] = "enabled"
 		self.save()
 		await ctx.message.add_reaction("👍")
@@ -47,26 +44,33 @@ class AntiPurge:
 	async def _disable(self, ctx):
 		guild_id = str(ctx.guild.id)
 		if guild_id not in self.toggle:
-			self.toggle[guild_id] = "disabled"
-			self.save()
-			return await ctx.message.add_reaction("👍")
-		self.toggle[guild_id] = "disabled"
+			return await ctx.send("Anti-Purge isn't enabled")
+		del self.toggle[guild_id]
 		self.save()
 		await ctx.message.add_reaction("👍")
 
 	async def on_member_remove(self, m):
 		guild_id = str(m.guild.id)
-		if guild_id not in self.cd:
-			self.cd[guild_id] = 0
-		self.cd[guild_id] = time.time() + 10
-		if self.cd[guild_id] > time.time() + 21:
-			perms = discord.Permissions
-			perms.administrator = False
-			perms.kick_members = False
-			perms.ban_members = False
-			for role in m.guild.roles:
-				if role.position < self.bot.get_guild(m.guild.id).get_member(self.bot.user.id).top_role.position:
-					await role.edit(permissions=perms)
+		if guild_id in self.toggle:
+			async for entry in m.guild.audit_logs(limit=1, after=time.time() - 2):
+				if str(entry.action) == "AuditLogAction.kick":
+					user_id = str(entry.user.id)
+					if guild_id not in self.cd:
+						self.cd[guild_id] = {}
+					if user_id not in self.cd[guild_id]:
+						self.cd[guild_id][user_id] = 0
+					self.cd[guild_id][user_id] = time.time() + 10
+					if self.cd[guild_id][user_id] > time.time() + 21:
+						await entry.user.ban(reason="Purging")
+				if str(entry.action) == "AuditLogAction.ban":
+					user_id = str(entry.user.id)
+					if guild_id not in self.cd:
+						self.cd[guild_id] = {}
+					if user_id not in self.cd[guild_id]:
+						self.cd[guild_id][user_id] = 0
+					self.cd[guild_id][user_id] = time.time() + 10
+					if self.cd[guild_id][user_id] > time.time() + 21:
+						await entry.user.ban(reason="Purging")
 
 def setup(bot):
 	bot.add_cog(AntiPurge(bot))
