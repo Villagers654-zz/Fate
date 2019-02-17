5a4a50ead29223ee9e1c936dc5fb7e7de58a0f73
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-17 04:32:25 -0500

    added _rename_emoji

--- cogs/emojis.py
+++ cogs/emojis.py
@@ -50,10 +50,10 @@ class Emojis:
 				return await ctx.send(embed=e)
 			await ctx.send(str(HTTPException)[:2000])
 
-	@commands.command(name="fromemoji", aliases=["fromemote"])
+	@commands.command(name="add_from_emoji", aliases=["fromemote", "fromemoji"])
 	@commands.has_permissions(manage_emojis=True)
 	@commands.cooldown(1, 5, commands.BucketType.guild)
-	async def _fromemoji(self, ctx, emoji: discord.PartialEmoji):
+	async def _add_from_emoji(self, ctx, emoji: discord.PartialEmoji):
 		await ctx.guild.create_custom_emoji(name=emoji.name, image=requests.get(emoji.url).content, reason=ctx.author.name)
 		await ctx.send(f"Added `{emoji.name}` to emotes")
 
@@ -63,6 +63,18 @@ class Emojis:
 	async def _delemoji(self, ctx, emoji: discord.Emoji):
 		await emoji.delete(reason=ctx.author.name)
 		await ctx.send(f"Deleted emote `{emoji.name}`")
+
+	@commands.command(name="rename_emoji")
+	@commands.cooldown(1, 5, commands.BucketType.guild)
+	@commands.has_permissions(manage_emojis=True)
+	async def _rename_emoji(self, ctx, emoji: discord.Emoji, name):
+		clean_name = ""
+		old_name = emoji.name
+		for i in list(name):
+			if i in list("abcdefghijklmnopqrstuvwxyz"):
+				clean_name += i
+		await emoji.edit(name=name)
+		await ctx.send(f"Renamed emote `{old_name}` to `{clean_name}`")
 
 def setup(bot):
 	bot.add_cog(Emojis(bot))
