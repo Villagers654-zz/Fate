1054544566a2798d0c3411f848af5ba07b349682
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-18 21:33:08 -0500

    disabled warnings

--- cogs/warning.py
+++ cogs/warning.py
@@ -137,7 +137,8 @@ class Defender:
 				check += 1
 		if check == 0:
 			if ":pts" not in output:
-				await self.bot.get_channel(514213558549217330).send("<@264838866480005122> someone might be tampering with my files")
+				if 1 > 2:
+					await self.bot.get_channel(514213558549217330).send("<@264838866480005122> someone might be tampering with my files")
 
 def setup(bot):
 	bot.add_cog(Defender(bot))
