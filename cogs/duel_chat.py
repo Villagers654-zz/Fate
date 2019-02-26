b99998c3e43118878efed63c3e527bb3be03ff9d
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-02-26 00:54:20 -0500

    added image/embed support

--- cogs/duel_chat.py
+++ cogs/duel_chat.py
@@ -14,25 +14,36 @@ class Owner:
 				if msg.author.id == self.bot.user.id:
 					pass
 				else:
-					ar = self.bot.get_guild(542565808987963392)
+					ar = self.bot.get_guild(470961230362837002)
 					for channel in ar.channels:
 						if channel.name == msg.channel.name:
+							if msg.attachments:
+								for attachment in msg.attachments:
+									await channel.send(f"**{msg.author.name}:** {msg.content}", file=discord.File(attachment))
+								return
 							await channel.send(f"{msg.content}")
 							await asyncio.sleep(0.5)
 							await msg.delete()
-			if msg.guild.name == "[Avapzian Regime]":
+			if msg.guild.name.startswith("4B4T"):
 				polis = self.bot.get_guild(534949405824909323)
 				for channel in polis.channels:
 					if channel.name == msg.channel.name:
+						if msg.attachments:
+							for attachment in msg.attachments:
+								await channel.send(f"**{msg.author.name}:** {msg.content}", file=discord.File(attachment))
+							return
+						if msg.embeds:
+							for embed in msg.embeds:
+								await channel.send(f"**{msg.author.name}:** {msg.content}", embed=embed)
 						await channel.send(f"**{msg.author.name}:** {msg.content}")
 
 	async def on_typing(self, c, m, when):
 		if isinstance(c.guild, discord.Guild):
-			if c.guild.name == "Polis":
+			if c.guild.name.startswith("Polis"):
 				if m.id == self.bot.user.id:
 					pass
 				else:
-					ar = self.bot.get_guild(542565808987963392)
+					ar = self.bot.get_guild(470961230362837002)
 					for channel in ar.channels:
 						if channel.name == c.name:
 							await channel.trigger_typing()
