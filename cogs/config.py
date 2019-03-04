58458fd82feea0c9d8c99e44bd05b831c424e1e3
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-03-04 08:58:13 -0500

    added _config

---
+++ cogs/config.py
@@ -0,0 +1,78 @@
+from discord.ext import commands
+from utils import colors
+import discord
+import json
+
+class Config:
+	def __init__(self, bot):
+		self.bot = bot
+
+	def prefix(self, id):
+		with open("./data/userdata/prefixes.json", "r") as f:
+			dat = json.load(f)
+			if id in dat:
+				return dat[id]
+			return "."
+
+	def anti_spam(self, id):
+		with open("./data/userdata/anti_spam.json", "r") as f:
+			if id in json.load(f)["toggle"]:
+				return "active"
+			return "inactive"
+
+	def anti_raid(self, id):
+		with open("./data/userdata/anti_spam.json", "r") as f:
+			if id in json.load(f)["toggle"]:
+				return "active"
+			return "inactive"
+
+	def anti_purge(self, id):
+		with open("./data/userdata/anti_spam.json", "r") as f:
+			if id in json.load(f)["toggle"]:
+				return "active"
+			return "inactive"
+
+	def welcome(self, id):
+		with open("./data/userdata/config/welcome.json", "r") as f:
+			if id in json.load(f)["identifier"]:
+				return "active"
+			return "inactive"
+
+	def farewell(self, id):
+		with open("./data/userdata/config/farewell.json", "r") as f:
+			if id in json.load(f)["identifier"]:
+				return "active"
+			return "inactive"
+
+	def logger(self, id):
+		with open("./data/userdata/logger.json", "r") as f:
+			if id in json.load(f)["channel"]:
+				return "active"
+			return "inactive"
+
+	def lock(self, id):
+		with open("./data/userdata/lock.json", "r") as f:
+			if id in json.load(f)["lock"]:
+				return "active"
+			return "inactive"
+
+	@commands.command(name="config")
+	@commands.bot_has_permissions(embed_links=True)
+	async def _config(self, ctx):
+		guild_id = str(ctx.guild.id)
+		e = discord.Embed(color=colors.fate())
+		e.set_author(name="| 💎 Server Config 💎", icon_url=ctx.guild.owner.avatar_url)
+		e.set_thumbnail(url=ctx.guild.icon_url)
+		e.description = f"**Prefix:** [`{self.prefix(guild_id)}`]\n"
+		module_config =  f"**Anti Spam:** [`{self.anti_spam(guild_id)}`]\n" \
+			f"**Anti Raid:** [`{self.anti_raid(guild_id)}`]\n" \
+			f"**Anti Purge:** [`{self.anti_purge(guild_id)}`]\n" \
+			f"**Welcome:** [`{self.welcome(guild_id)}`]\n" \
+			f"**Farewell:** [`{self.farewell(guild_id)}`]\n" \
+			f"**Logger:** [`{self.logger(guild_id)}`]\n" \
+			f"**Lock:** [`{self.lock(guild_id)}`]"
+		e.add_field(name="◈ Modules ◈", value=module_config)
+		await ctx.send(embed=e)
+
+def setup(bot):
+	bot.add_cog(Config(bot))
