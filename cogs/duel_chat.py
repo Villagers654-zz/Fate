e68dbca4542bb8cce423cde63aa3d95825861040
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-03-13 16:31:08 -0400

    repairs

--- cogs/duel_chat.py
+++ cogs/duel_chat.py
@@ -11,36 +11,41 @@ class Owner:
 
 	async def on_message(self, msg: discord.Message):
 		if isinstance(msg.guild, discord.Guild):
-			if msg.guild.name == "Polis":
-				if msg.author.id == self.bot.user.id:
-					pass
-				else:
-					ar = self.bot.get_guild(470961230362837002)
-					for channel in ar.channels:
+			try:
+				if msg.guild.name == "Polis":
+					if msg.author.id == self.bot.user.id:
+						pass
+					else:
+						ar = self.bot.get_guild(470961230362837002)
+						for channel in ar.channels:
+							if channel.name == msg.channel.name:
+								if msg.attachments:
+									for attachment in msg.attachments:
+										await channel.send(f"**{msg.author.name}:** {msg.content}",
+										                   file=discord.File(requests.get(attachment).content))
+									return
+								await channel.send(f"{msg.content}")
+								await asyncio.sleep(0.5)
+								await msg.delete()
+				if msg.guild.name.startswith("4B4T"):
+					polis = self.bot.get_guild(534949405824909323)
+					for channel in polis.channels:
 						if channel.name == msg.channel.name:
 							if msg.attachments:
 								for attachment in msg.attachments:
-									await channel.send(f"**{msg.author.name}:** {msg.content}", file=discord.File(requests.get(attachment).content))
+									await channel.send(f"**{msg.author.name}:** {msg.content}",
+									                   file=discord.File(requests.get(attachment).content))
 								return
-							await channel.send(f"{msg.content}")
-							await asyncio.sleep(0.5)
-							await msg.delete()
-			if msg.guild.name.startswith("4B4T"):
-				polis = self.bot.get_guild(534949405824909323)
-				for channel in polis.channels:
-					if channel.name == msg.channel.name:
-						if msg.attachments:
-							for attachment in msg.attachments:
-								await channel.send(f"**{msg.author.name}:** {msg.content}", file=discord.File(requests.get(attachment).content))
-							return
-						if msg.embeds:
-							for embed in msg.embeds:
-								await channel.send(f"**{msg.author.name}:** {msg.content}", embed=embed)
-							return
-						await channel.send(f"**{msg.author.name}:** {msg.content}")
+							if msg.embeds:
+								for embed in msg.embeds:
+									await channel.send(f"**{msg.author.name}:** {msg.content}", embed=embed)
+								return
+							await channel.send(f"**{msg.author.name}:** {msg.content}")
+			except:
+				pass
 
 	async def on_typing(self, c, m, when):
-		if isinstance(c.guild, discord.Guild):
+		if isinstance(c, discord.TextChannel):
 			if c.guild.name.startswith("Polis"):
 				if m.id == self.bot.user.id:
 					pass
