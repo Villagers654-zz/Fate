409d2d6a29cbbfd521a02cc029c21e232678810b
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-03-25 22:47:47 -0400

    rewrote farewell.py

--- cogs/farewell.py
+++ cogs/farewell.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from os.path import isfile
+from utils import colors
 import discord
 import random
 import json
@@ -8,177 +9,169 @@ import os
 class Farewell:
 	def __init__(self, bot):
 		self.bot = bot
-		self.identifier = {}
+		self.toggle = {}
 		self.channel = {}
 		self.useimages = {}
-		self.toxicity = {}
-		if isfile("./data/userdata/config/farewell.json"):
-			with open("./data/userdata/config/farewell.json", "r") as infile:
-				dat = json.load(infile)
-				if "identifier" in dat and "channel" in dat and "useimages" in dat and "toxicity" in dat:
-					self.identifier = dat["identifier"]
+		self.format = {}
+		if isfile("./data/userdata/farewell.json"):
+			with open("./data/userdata/farewell.json", "r") as f:
+				dat = json.load(f)
+				if "toggle" in dat:
+					self.toggle = dat["toggle"]
 					self.channel = dat["channel"]
 					self.useimages = dat["useimages"]
-					self.toxicity = dat["toxicity"]
+					self.format = dat["format"]
 
-	def save(self):
-		with open("./data/userdata/config/farewell.json", "w") as outfile:
-			json.dump({"identifier": self.identifier, "channel": self.channel, "useimages": self.useimages,
-			           "toxicity": self.toxicity}, outfile, ensure_ascii=False)
+	def save_data(self):
+		with open("./data/userdata/farewell.json", "w") as f:
+			json.dump({"toggle": self.toggle, "channel": self.channel, "useimages": self.useimages,
+			    "format": self.format}, f, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
-	@commands.group(name='farewell')
-	@commands.has_permissions(manage_guild=True)
+	@commands.group(name="farewell", description="Gives a farewell in chat when a user leaves the guild")
+	@commands.cooldown(1, 3, commands.BucketType.channel)
+	@commands.guild_only()
+	@commands.bot_has_permissions(embed_links=True)
 	async def _farewell(self, ctx):
-		async with ctx.typing():
-			if ctx.invoked_subcommand is None:
-				await ctx.send('**Farewell Message Instructions:**\n'
-				               '.farewell enable ~ `enables welcome messages`\n'
-				               '.farewell disable ~ `disables welcome messages`\n'
-				               '.farewell setchannel ~ `sets the channel`\n'
-				               '.farewell useimages ~ `true or false`\n'
-				               '.farewell toxicity ~ `true or false`')
+		if not ctx.invoked_subcommand:
+			guild_id = str(ctx.guild.id)
+			toggle = "disabled"
+			if guild_id in self.toggle:
+				toggle = "enabled"
+			e = discord.Embed(color=colors.tan())
+			e.set_author(name="Farewell Messages", icon_url=self.bot.user.avatar_url)
+			e.set_thumbnail(url=ctx.guild.icon_url)
+			e.description = "Gives a farewell in chat when a user leaves the guild"
+			e.add_field(name="◈ Basic Usage ◈", value=
+				".farewell enable\n"
+				".farewell disable\n"
+				".farewell config\n"
+				".farewell setchannel {channel}\n"
+				".farewell useimages\n"
+				".farewell format {message}\n", inline=False)
+			e.add_field(name="◈ Msg Format ◈", value=
+				"$NAME\n"
+				"`uses a users name`\n"
+				"$USER\n"
+				"`uses a users name & tag\n"
+				"**Example:**\n"
+				"`.farewell format Cya $NAME`", inline=False)
+			e.set_footer(text=f"Current Status: {toggle}", icon_url=ctx.guild.owner.avatar_url)
+			await ctx.send(embed=e)
 
-	@_farewell.command(name='toggle')
-	async def _toggle(self, ctx):
-		"""Not in use, but still works"""
-		report = ""
-		if str(ctx.guild.id) not in self.identifier:
-			self.identifier[str(ctx.guild.id)] = 'True'
-			report += 'Enabled farewell messages'
-		else:
-			if self.identifier[str(ctx.guild.id)] == 'True':
-				self.identifier[str(ctx.guild.id)] = 'False'
-				report += 'Disabled farewell messages'
-			else:
-				if self.identifier[str(ctx.guild.id)] == 'False':
-					self.identifier[str(ctx.guild.id)] = 'True'
-					report += 'Enabled farewell messages'
-		if str(ctx.guild.id) not in self.channel:
-			self.channel[str(ctx.guild.id)] = ctx.channel.id
-			report += f'\nFarewell channel not set, therefore it has been automatically set to `{ctx.channel.name}`'
-		if str(ctx.guild.id) not in self.useimages:
-			self.useimages[str(ctx.guild.id)] = 'False'
-			report += '\nUseimages not set, therefore it has been automatically set to `false`'
-		if str(ctx.guild.id) not in self.toxicity:
-			self.toxicity[str(ctx.guild.id)] = 'False'
-			report += '\nToxicity not set, therefore it has been automatically set to `false`'
-		self.save()
-		await ctx.send(report)
-
-	@_farewell.command(name='enable')
+	@_farewell.command(name="enable")
+	@commands.has_permissions(manage_guild=True)
 	async def _enable(self, ctx):
-		report = ""
-		if str(ctx.guild.id) not in self.identifier:
-			self.identifier[str(ctx.guild.id)] = 'True'
-			report += 'Enabled farewell messages'
-		else:
-			self.identifier[str(ctx.guild.id)] = 'True'
-			report += 'Enabled farewell messages'
-		if str(ctx.guild.id) not in self.channel:
-			self.channel[str(ctx.guild.id)] = ctx.channel.id
-			report += f'\nFarewell channel not set, therefore it has been automatically set to `{ctx.channel.name}`'
-		if str(ctx.guild.id) not in self.useimages:
-			self.useimages[str(ctx.guild.id)] = 'False'
-			report += '\nUseimages not set, therefore it has been automatically set to `false`'
-		if str(ctx.guild.id) not in self.toxicity:
-			self.toxicity[str(ctx.guild.id)] = 'False'
-			report += '\nToxicity not set, therefore it has been automatically set to `false`'
-		self.save()
-		await ctx.send(report)
+		guild_id = str(ctx.guild.id)
+		if guild_id in self.toggle:
+			return await ctx.send("This module is already enabled")
+		self.toggle[guild_id] = "enabled"
+		e = discord.Embed(color=colors.tan())
+		e.set_author(name="Enabled Farewell Messages", icon_url=ctx.author.avatar_url)
+		e.set_thumbnail(url=ctx.guild.icon_url)
+		if guild_id not in self.channel:
+			self.channel[guild_id] = ctx.channel.id
+		if guild_id not in self.useimages:
+			self.useimages[guild_id] = "disabled"
+		if guild_id not in self.format:
+			self.format[guild_id] = "Cya $USER"
+		e.description = \
+			f"**Channel:** {self.bot.get_channel(self.channel[guild_id]).mention}\n" \
+			f"**UseImages:** {self.useimages[guild_id]}\n" \
+			f"**Format:** `{self.format[guild_id]}`\n"
+		await ctx.send(embed=e)
+		self.save_data()
 
-	@_farewell.command(name='disable')
+	@_farewell.command(name="disable")
+	@commands.has_permissions(manage_guild=True)
 	async def _disable(self, ctx):
-		report = ""
-		if str(ctx.guild.id) not in self.identifier:
-			self.identifier[str(ctx.guild.id)] = 'False'
-			report += 'Disabled farewell messages'
-		else:
-			self.identifier[str(ctx.guild.id)] = 'False'
-			report += 'Disabled farewell messages'
-		self.save()
-		await ctx.send(report)
+		guild_id = str(ctx.guild.id)
+		if guild_id not in self.toggle:
+			return await ctx.send("This module isn't enabled")
+		del self.toggle[guild_id]
+		await ctx.send("Disabled welcome messages")
+		self.save_data()
 
-	@_farewell.command(name='setchannel')
+	@_farewell.command(name="config")
+	async def _config(self, ctx):
+		guild_id = str(ctx.guild.id)
+		toggle = "disabled"
+		channel = "none"
+		useimages = "disabled"
+		format = "none"
+		if guild_id in self.toggle:
+			toggle = "enabled"
+		if guild_id in self.channel:
+			channel = self.bot.get_channel(self.channel[guild_id]).mention
+		if guild_id in self.useimages:
+			useimages = self.useimages[guild_id]
+		if guild_id in self.format:
+			format = self.format[guild_id]
+		e = discord.Embed(color=colors.tan())
+		e.set_author(name="Farewell Config", icon_url=self.bot.user.avatar_url)
+		e.set_thumbnail(url=ctx.guild.icon_url)
+		e.description = f"**Toggle:** {toggle}\n" \
+			f"**Channel:** {channel}\n" \
+			f"**UseImages:** {useimages}\n" \
+			f"**Format:** {format}\n"
+		await ctx.send(embed=e)
+
+	@_farewell.command(name="setchannel")
+	@commands.has_permissions(manage_guild=True)
 	async def _setchannel(self, ctx, channel: discord.TextChannel=None):
-		if channel is None:
+		guild_id = str(ctx.guild.id)
+		if not channel:
 			channel = ctx.channel
-		self.channel[str(ctx.guild.id)] = channel.id
-		self.save()
-		await ctx.send(f'Set the farewell channel to `{channel.name}`')
+		self.channel[guild_id] = channel.id
+		await ctx.send(f"Set the farewell message channel to {channel.mention}")
+		self.save_data()
 
-	@_farewell.command(name='useimages')
-	async def _useimages(self, ctx, toggle=None):
-		if toggle is None:
-			if str(ctx.guild.id) not in self.useimages:
-				self.useimages[str(ctx.guild.id)] = 'True'
-				await ctx.send('Enabled `useimages`')
-			else:
-				if self.useimages[str(ctx.guild.id)] == 'False':
-					self.useimages[str(ctx.guild.id)] = 'True'
-					await ctx.send('Enabled `useimages`')
-				else:
-					self.useimages[str(ctx.guild.id)] = 'False'
-					await ctx.send('Disabled `useimages`')
-		else:
-			toggle = toggle.lower()
-			if toggle == 'true':
-				self.useimages[str(ctx.guild.id)] = 'True'
-				await ctx.send('Enabled `useimages`')
-			else:
-				if toggle == 'false':
-					self.useimages[str(ctx.guild.id)] = 'False'
-					await ctx.send('Disabled `useimages`')
-		self.save()
+	@_farewell.command(name="useimages")
+	@commands.has_permissions(manage_guild=True)
+	@commands.bot_has_permissions(attach_files=True)
+	async def _useimages(self, ctx):
+		guild_id = str(ctx.guild.id)
+		if guild_id in self.useimages:
+			del self.useimages[guild_id]
+			return await ctx.send("Disabled UseImages")
+		self.useimages[guild_id] = "enabled"
+		await ctx.send("Enabled UseImages")
+		self.save_data()
 
-	@_farewell.command(name='toxicity')
-	async def _toxicity(self, ctx, toggle=None):
-		if toggle is None:
-			if str(ctx.guild.id) not in self.useimages:
-				self.useimages[str(ctx.guild.id)] = 'True'
-				await ctx.send('Enabled `toxicity`')
-			else:
-				if self.useimages[str(ctx.guild.id)] == 'False':
-					self.useimages[str(ctx.guild.id)] = 'True'
-					await ctx.send('Enabled `toxicity`')
-				else:
-					self.useimages[str(ctx.guild.id)] = 'False'
-					await ctx.send('Disabled `toxicity`')
-		else:
-			toggle = toggle.lower()
-			if toggle == 'true':
-				self.useimages[str(ctx.guild.id)] = 'True'
-				await ctx.send('Enabled `toxicity`')
-			else:
-				if toggle == 'false':
-					self.useimages[str(ctx.guild.id)] = 'False'
-					await ctx.send('Disabled `toxicity`')
-		self.save()
+	@_farewell.command(name="format")
+	@commands.has_permissions(manage_guild=True)
+	async def _format(self, ctx, *, message):
+		guild_id = str(ctx.guild.id)
+		self.format[guild_id] = message
+		await ctx.message.add_reaction("👍")
+		self.save_data()
 
-	async def on_member_remove(self, member: discord.Member):
-		guild_id = str(member.guild.id)
-		if guild_id in self.identifier:
-			if self.identifier[str(member.guild.id)] == 'True':
-				message = None
-				channel = self.bot.get_channel(self.channel[guild_id])
-				path = os.getcwd() + "/data/images/reactions/farewell/" + random.choice(os.listdir(os.getcwd() + "/data/images/reactions/farewell/"))
-				e = discord.Embed(color=0x80b0ff)
-				if self.toxicity[str(member.guild.id)] == 'True':
-					toxicity = random.choice([
-						"and good riddance", "and please never come back", "I've always gagged every time I read one of your messages", "never come back"])
-					message = f'Cya {member.name}, {toxicity}'
-				else:
-					e.set_author(name=f'Cya {member.name}')
-				if self.useimages[str(member.guild.id)] == 'True':
+	async def on_member_remove(self, m: discord.Member):
+		if isinstance(m.guild, discord.Guild):
+			guild_id = str(m.guild.id)
+			if guild_id in self.toggle:
+				try:
+					channel = self.bot.get_channel(self.channel[guild_id])
+				except:
+					del self.toggle[guild_id]
+					return self.save_data()
+				msg = self.format[guild_id]
+				msg = msg.replace("$NAME", m.name).replace("$USER", m)
+				path = os.getcwd() + "/data/images/reactions/farewell/" + random.choice(
+					os.listdir(os.getcwd() + "/data/images/reactions/farewell/"))
+				if guild_id in self.useimages:
+					e = discord.Embed(color=colors.fate())
 					e.set_image(url="attachment://" + os.path.basename(path))
-					if message == None:
-						await channel.send(file=discord.File(path, filename=os.path.basename(path)), embed=e)
-					else:
-						await channel.send(message, file=discord.File(path, filename=os.path.basename(path)), embed=e)
+					try:
+						await channel.send(msg, file=discord.File(path, filename=os.path.basename(path)), embed=e)
+					except:
+						del self.useimages[guild_id]
+						self.save_data()
 				else:
-					if message == None:
-						await channel.send(embed=e)
-					else:
-						await channel.send(message)
+					try:
+						await channel.send(msg)
+					except:
+						del self.toggle[guild_id]
+						self.save_data()
 
 def setup(bot):
 	bot.add_cog(Farewell(bot))
