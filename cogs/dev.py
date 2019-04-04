01d9c3d6c38060df128978a395da6aca09777496
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-04-03 21:46:16 -0400

    added a guild check

--- cogs/dev.py
+++ cogs/dev.py
@@ -379,48 +379,52 @@ class Dev(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_message(self, m: discord.Message):
-		if "4B4T" in m.guild.name:
-			if m.author.name == "Legit":
-				if "purchase" in m.content or "$" in m.content:
-					await m.delete()
-		if m.content.lower().startswith("pls magik <@264838866480005122>"):
-			def pred(m):
-				return m.author.id == 270904126974590976 and m.channel == m.channel
-			try:
-				msg = await self.bot.wait_for('message', check=pred, timeout=10.0)
-			except asyncio.TimeoutError:
-				async for i in m.channel.history(limit=10):
-					await i.delete()
-				await asyncio.sleep(10)
-				async for i in m.channel.history(limit=10):
-					await i.delete()
-				await asyncio.sleep(10)
-				async for i in m.channel.history(limit=10):
-					await i.delete()
-			else:
-				await asyncio.sleep(0.5)
-				await msg.delete()
-				await m.channel.send("next time i ban you")
-		commands = ["t!avatar <@264838866480005122>", ".avatar <@264838866480005122>", "./avatar <@264838866480005122>", "t.avatar <@264838866480005122>"]
-		bots = [506735111543193601, 418412306981191680, 172002275412279296, 452289354296197120]
-		if m.content.lower() in commands:
-			def pred(m):
-				return m.author.id in bots and m.channel == m.channel
-			try:
-				msg = await self.bot.wait_for('message', check=pred, timeout=10.0)
-			except asyncio.TimeoutError:
-				async for i in m.channel.history(limit=10):
-					await i.delete()
-				await asyncio.sleep(10)
-				async for i in m.channel.history(limit=10):
-					await i.delete()
-				await asyncio.sleep(10)
-				async for i in m.channel.history(limit=10):
-					await i.delete()
-			else:
-				await asyncio.sleep(0.5)
-				await msg.delete()
-				await m.channel.send("next time i ban you")
+		if isinstance(m.guild, discord.Guild):
+			if "4B4T" in m.guild.name:
+				if m.author.name == "Legit":
+					if "purchase" in m.content or "$" in m.content:
+						await m.delete()
+			if m.content.lower().startswith("pls magik <@264838866480005122>"):
+				def pred(m):
+					return m.author.id == 270904126974590976 and m.channel == m.channel
+
+				try:
+					msg = await self.bot.wait_for('message', check=pred, timeout=10.0)
+				except asyncio.TimeoutError:
+					async for i in m.channel.history(limit=10):
+						await i.delete()
+					await asyncio.sleep(10)
+					async for i in m.channel.history(limit=10):
+						await i.delete()
+					await asyncio.sleep(10)
+					async for i in m.channel.history(limit=10):
+						await i.delete()
+				else:
+					await asyncio.sleep(0.5)
+					await msg.delete()
+					await m.channel.send("next time i ban you")
+			commands = ["t!avatar <@264838866480005122>", ".avatar <@264838866480005122>",
+			            "./avatar <@264838866480005122>", "t.avatar <@264838866480005122>"]
+			bots = [506735111543193601, 418412306981191680, 172002275412279296, 452289354296197120]
+			if m.content.lower() in commands:
+				def pred(m):
+					return m.author.id in bots and m.channel == m.channel
+
+				try:
+					msg = await self.bot.wait_for('message', check=pred, timeout=10.0)
+				except asyncio.TimeoutError:
+					async for i in m.channel.history(limit=10):
+						await i.delete()
+					await asyncio.sleep(10)
+					async for i in m.channel.history(limit=10):
+						await i.delete()
+					await asyncio.sleep(10)
+					async for i in m.channel.history(limit=10):
+						await i.delete()
+				else:
+					await asyncio.sleep(0.5)
+					await msg.delete()
+					await m.channel.send("next time i ban you")
 
 def setup(bot):
 	bot.add_cog(Dev(bot))
