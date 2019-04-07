d96bc1756d0e139e16a02181bbce1cb6f1c93f6a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-04-07 10:13:44 -0400

    removed msg listener

--- cogs/team.py
+++ cogs/team.py
@@ -55,15 +55,5 @@ class AvapxianRegime(commands.Cog):
 			e.set_footer(text=f"Last Updated: {datetime.datetime.now().strftime(fmt)}")
 			await msg.edit(embed=e)
 			return await ctx.message.delete()
-
-	@commands.Cog.listener()
-	async def on_message(self, m: discord.Message):
-		if isinstance(m.guild, discord.Guild):
-			channel = self.bot.get_channel(562384658596364311)
-			if m.guild.id == 562380812230590531:
-				if m.attachments:
-					return await channel.send(f"**{m.channel.name} | {m.author.name}:** {m.content}", file=discord.File(BytesIO(requests.get(m.attachments[0]).content)))
-				await channel.send(f"**{m.channel.name} | {m.author.name}:** {m.content}")
-
 def setup(bot):
 	bot.add_cog(AvapxianRegime(bot))
