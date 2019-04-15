8a2dc932c1b2ec0ec35164dac5607e9362493af2
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-04-15 19:53:18 -0400

    added can_dm

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,15 @@
+from discord.ext import commands
 import datetime
+import discord
 import time
+
+class Bot:
+
+	async def can_dm(self, user: discord.User):
+		dm_channel = user.dm_channel
+		if not dm_channel:
+			dm_channel = await user.create_dm()
+		return dm_channel.permissions_for(self).send_messages
 
 class Datetime:
 	def __init__(self, seconds):
