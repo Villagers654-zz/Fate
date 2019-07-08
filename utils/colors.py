5645761c242458f86c2530f15fea1e6d0c48b25a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-07-08 17:33:54 -0400

    cleanup

--- utils/colors.py
+++ utils/colors.py
@@ -1,5 +1,4 @@
 import random as r
-import json
 
 def fate():
 	return 0x80b0ff
@@ -45,3 +44,45 @@ def white():
 
 def tan():
 	return 0xffc8b0
+
+class ColorSets:
+
+	def rainbow(self):
+		return [
+        0xff0000,
+        0xff002a,
+        0xff0055,
+        0xff007f,
+        0xff00aa,
+        0xff00d4,
+        0xff00ff,
+        0xd500ff,
+        0xaa00ff,
+        0x8000ff,
+        0x5500ff,
+        0x2b00ff,
+        0x0000ff,
+        0x002aff,
+        0x0055ff,
+        0x007fff,
+        0x00aaff,
+        0x00d4ff,
+        0x00ffff,
+        0x00ffd5,
+        0x00ffaa,
+        0x00ff80,
+        0x00ff55,
+        0x00ff2b,
+        0x00ff00,
+        0x2aff00,
+        0x55ff00,
+        0x7fff00,
+        0xaaff00,
+        0xd4ff00,
+        0xffff00,
+        0xffd500,
+        0xffaa00,
+        0xff8000,
+        0xff5500,
+        0xff2b00,
+    ]
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+from utils import colors
 import datetime
 import discord
 import asyncio
@@ -13,6 +14,88 @@ def bytes2human(n):
 			value = float(n) / prefix[s]
 			return '%.1f%s' % (value, s)
 	return "%sB" % n
+
+def get_user(ctx, user):
+	if not user:
+		return ctx.author
+	if user.startswith("<@"):
+		for char in list(user):
+			if char not in list('1234567890'):
+				user = user.replace(str(char), '')
+		return ctx.guild.get_member(int(user))
+	else:
+		user = user.lower()
+		for member in ctx.guild.members:
+			if user == member.name.lower():
+				return member
+		for member in ctx.guild.members:
+			if user == member.display_name.lower():
+				return member
+		for member in ctx.guild.members:
+			if user in member.name.lower():
+				return member
+		for member in ctx.guild.members:
+			if user in member.display_name.lower():
+				return member
+	return None
+
+async def get_role(ctx, name):
+	if name.startswith("<@"):
+		for char in list(name):
+			if char not in list('1234567890'):
+				name = name.replace(str(char), '')
+		return ctx.guild.get_member(int(name))
+	else:
+		roles = []
+		for role in ctx.guild.roles:
+			if name == role.name.lower():
+				roles.append(role)
+		if not roles:
+			for role in ctx.guild.roles:
+				if name in role.name.lower():
+					roles.append(role)
+		if roles:
+			if len(roles) == 1:
+				return roles[0]
+			index = 1
+			role_list = ''
+			for role in roles:
+				role_list += f'{index} : {role.mention}\n'
+				index += 1
+			e = discord.Embed(color=colors.fate(), description=role_list)
+			e.set_author(name='Multiple Roles Found')
+			e.set_footer(text='Reply with the correct role number')
+			embed = await ctx.send(embed=e)
+			def pred(m):
+				return m.channel.id == ctx.channel.id and m.author.id == ctx.author.id
+			try:
+				msg = await ctx.bot.wait_for('message', check=pred, timeout=60)
+			except asyncio.TimeoutError:
+				await ctx.send('Timeout error', delete_after=5)
+				return await embed.delete()
+			else:
+				try:
+					role = int(msg.content)
+				except:
+					return await ctx.send('Invalid response')
+				if role > len(roles):
+					return await ctx.send('Invalid response')
+				await embed.delete()
+				await msg.delete()
+				return roles[role - 1]
+
+async def wait_for_msg(self, ctx, user=None):
+	if not user:
+		user = ctx.author
+	def pred(m):
+		return m.channel.id == ctx.channel.id and m.author.id == user.id
+	try:
+		msg = await self.bot.wait_for('message', check=pred, timeout=60)
+	except asyncio.TimeoutError:
+		await ctx.send("Timeout error")
+		return False
+	else:
+		return msg
 
 class Bot:
 	def __init__(self, bot):
