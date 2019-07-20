a883991395e8ce0c706918d8c7b89964c1a88ddb
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-07-20 00:36:16 -0400

    cleanup

--- cogs/core.py
+++ cogs/core.py
@@ -133,7 +133,7 @@ class Core(commands.Cog):
 		e.description = f"**Message Trip 1:** `{int(ping)}ms`\n**Message Trip 2:** `{int(second_ping)}ms`\n**Msg Edit Trip 1:** `{int(edit_ping)}ms`\n**Msg Edit Trip 2:** `{int(second_edit_ping)}ms`\n**Websocket Heartbeat:** `{api}ms`"
 		await message.edit(embed=e)
 
-	@commands.command(name="wiki")
+	@commands.command(name='wiki')
 	@commands.cooldown(1, 5, commands.BucketType.user)
 	@commands.bot_has_permissions(embed_links=True)
 	async def wiki(self,ctx,*,query:str):
