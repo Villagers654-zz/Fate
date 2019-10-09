a9b871bdc5dde5887a02f909c958987e5e59c760
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-10-08 21:12:22 -0400

    added the ability to disable individual modules, cleaned up the help menu, and added spam protection for duplicate messages

--- cogs/anti_spam.py
+++ cogs/anti_spam.py
@@ -11,9 +11,11 @@ from utils import colors, utils
 class AntiSpam(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.spam_cd = {}
-        self.macro_cd = {}
-        self.ping_cd = {}
+        self.spam_cd = {}  # sending messages in mass
+        self.macro_cd = {}  # same interval between each msg
+        self.ping_cd = {}  # mass pings
+        self.dupes = {}
+        self.dupez = {}
         self.toggle = {}
         self.sensitivity = {}
         self.blacklist = {}
@@ -31,6 +33,7 @@ class AntiSpam(commands.Cog):
                 self.sensitivity = dat['sensitivity']
                 self.blacklist = dat['blacklist']
 
+
     def save_data(self):
         with open(self.path, 'w+') as f:
             json.dump({'toggle': self.toggle, 'sensitivity': self.sensitivity, 'blacklist': self.blacklist}, f)
@@ -42,7 +45,8 @@ class AntiSpam(commands.Cog):
         self.toggle[guild_id] = {
             'Rate-Limit': False,
             'Mass-Pings': False,
-            'Anti-Macro': False
+            'Anti-Macro': False,
+            'Duplicates': False
         }
 
 
@@ -55,14 +59,18 @@ class AntiSpam(commands.Cog):
             e = discord.Embed(color=colors.fate())
             e.set_author(name='AntiSpam Usage', icon_url=ctx.author.avatar_url)
             e.set_thumbnail(url=ctx.guild.icon_url)
-            e.description = '.anti-spam enable\n• enables all anti-spam modules\n\n' \
-                '.anti-spam enable rate-limit\n• prevents sending multiple msgs fast\n\n' \
-                '.anti-spam enable mass-pings\n• prevents mass pinging\n\n' \
-                '.anti-spam enable anti-macro\n• stops users from using macros for bots\n\n' \
-                '.anti-spam disable\n• disables all anti-spam modules\n\n' \
-                '.anti-spam alter-sensitivity\n• alters anti-spam sensitivity\n\n' \
-                '.anti-spam ignore #channel\n• ignores spam in a channel\n\n' \
-                '.anti-spam unignore #channel\n• no longer ignores a channels spam'
+            e.description = '**.anti-spam enable**\n`• enables all anti-spam modules`\n' \
+                '**.anti-spam enable module**\n`• enables a single module`\n' \
+                '**.anti-spam disable**\n`• disables all anti-spam modules`\n' \
+                '**.anti-spam disable module**\n`• disables a single module`\n' \
+                '**.anti-spam alter-sensitivity**\n`• alters anti-spam sensitivity`\n' \
+                '**.anti-spam ignore #channel**\n`• ignores spam in a channel`\n' \
+                '**.anti-spam unignore #channel**\n`• no longer ignores a channels spam`'
+            modules = '**Rate-Limit:** sending multiple msgs fast\n' \
+                '**Mass-Pings:** mass mentioning users\n' \
+                '**Anti-Macro:** using macros for bots\n' \
+                '**Duplicates:** copying and pasting the same msg'
+            e.add_field(name='Modules', value=modules)
             guild_id = str(ctx.guild.id)
             if guild_id in self.toggle:
                 conf = ''
@@ -83,13 +91,14 @@ class AntiSpam(commands.Cog):
             self.toggle[guild_id] = {
                 'Rate-Limit': True,
                 'Mass-Pings': True,
-                'Anti-Macro': True
+                'Anti-Macro': True,
+                'Duplicates': True
             }
             await ctx.send('Enabled all anti-spam modules')
             self.save_data()
 
     @_enable.command(name='rate-limit')
-    async def _rate_limit(self, ctx):
+    async def _enable_rate_limit(self, ctx):
         guild_id = str(ctx.guild.id)
         if guild_id not in self.toggle:
             self.init(guild_id)
@@ -98,7 +107,7 @@ class AntiSpam(commands.Cog):
         self.save_data()
 
     @_enable.command(name='mass-pings', aliases=['mass-ping'])
-    async def _mass_pings(self, ctx):
+    async def _enable_mass_pings(self, ctx):
         guild_id = str(ctx.guild.id)
         if guild_id not in self.toggle:
             self.init(guild_id)
@@ -107,7 +116,7 @@ class AntiSpam(commands.Cog):
         self.save_data()
 
     @_enable.command(name='anti-macro')
-    async def _anti_macro(self, ctx):
+    async def _enable_anti_macro(self, ctx):
         guild_id = str(ctx.guild.id)
         if guild_id not in self.toggle:
             self.init(guild_id)
@@ -115,14 +124,61 @@ class AntiSpam(commands.Cog):
         await ctx.send('Enabled anti-macro module')
         self.save_data()
 
-    @_enable.command(name='disable')
-    async def _disable(self, ctx):
+    @_enable.command(name='duplicates')
+    async def _enable_duplicates(self, ctx):
         guild_id = str(ctx.guild.id)
         if guild_id not in self.toggle:
-            return await ctx.send('Anti-Spam is\'nt enabled')
-        del self.toggle[guild_id]
-        del self.sensitivity[guild_id]
-        await ctx.send('Disabled anti-spam')
+            self.init(guild_id)
+        self.toggle[guild_id]['Duplicates'] = True
+        await ctx.send('Enabled duplicates module')
+        self.save_data()
+
+
+    @anti_spam.group(name='disable')
+    async def _disable(self, ctx):
+        if not ctx.invoked_subcommand:
+            guild_id = str(ctx.guild.id)
+            if guild_id not in self.toggle:
+                return await ctx.send('Anti-Spam is\'nt enabled')
+            del self.toggle[guild_id]
+            del self.sensitivity[guild_id]
+            await ctx.send('Disabled anti-spam')
+            self.save_data()
+
+    @_disable.command(name='rate-limit')
+    async def _disable_rate_limit(self, ctx):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.toggle:
+            self.init(guild_id)
+        self.toggle[guild_id]['Rate-Limit'] = False
+        await ctx.send('Disabled rate-limit module')
+        self.save_data()
+
+    @_disable.command(name='anti-macro')
+    async def _disable_anti_macro(self, ctx):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.toggle:
+            self.init(guild_id)
+        self.toggle[guild_id]['Anti-Macro'] = False
+        await ctx.send('Disabled anti-macro module')
+        self.save_data()
+
+    @_disable.command(name='mass-pings')
+    async def _disable_mass_pings(self, ctx):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.toggle:
+            self.init(guild_id)
+        self.toggle[guild_id]['Mass-Pings'] = False
+        await ctx.send('Disabled mass-pings module')
+        self.save_data()
+
+    @_disable.command(name='duplicates')
+    async def _disable_duplicates(self, ctx):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.toggle:
+            self.init(guild_id)
+        self.toggle[guild_id]['Duplicates'] = False
+        await ctx.send('Disabled duplicates module')
         self.save_data()
 
 
@@ -171,6 +227,7 @@ class AntiSpam(commands.Cog):
         if not isinstance(msg.guild, discord.Guild) or msg.author.bot:
             return
         guild_id = str(msg.guild.id)
+        channel_id = str(msg.channel.id)
         user_id = str(msg.author.id)
         triggered = False
         if guild_id in self.toggle:
@@ -228,6 +285,27 @@ class AntiSpam(commands.Cog):
                     if all(interval == intervals[0] for interval in intervals):
                         if self.toggle[guild_id]['Anti-Macro']:
                             triggered = True
+
+            # duplicate messages
+            if channel_id not in self.dupes:
+                self.dupes[channel_id] = []
+                self.dupez[channel_id] = []
+            self.dupes[channel_id].append([msg, time()])
+            self.dupes[channel_id] = self.dupes[channel_id][:10]
+            self.dupez[channel_id].append([msg, time()])
+            self.dupez[channel_id] = self.dupes[channel_id][:10]
+            data = [(m, m.content) for m, m_time in self.dupes[channel_id] if m_time > time() - 7 and len(m.content) > 3]
+            contents = [x[1] for x in data]
+            duplicates = [m for m in contents if contents.count(m) >= sensitivity_level]
+            if msg.content in duplicates:
+                data = [(m, m_time) for m, m_time in self.dupez[channel_id] if msg.content == m.content and [m, m_time] in data]
+                for m, m_time in data:
+                    self.dupez[channel_id].pop(self.dupez[channel_id].index([m, m_time]))
+                    if m in self.msgs[str(m.author.id)]:
+                        self.msgs[user_id].pop(self.msgs[user_id].index(m))
+                await msg.channel.delete_messages([m[1] for m in data])
+                if self.toggle[guild_id]['Duplicates']:
+                    triggered = True
 
             if triggered:
                 bot = msg.guild.me
