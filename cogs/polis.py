9bde90427c77732813596859c2f870c34b757dc5
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-11-11 21:25:39 -0500

    added more partners to polis

--- cogs/polis.py
+++ cogs/polis.py
@@ -30,22 +30,41 @@ class Sliders:
 		e.set_image(url=random.choice(image_urls))
 		return e
 
-	def luke(self):
-		e = discord.Embed(color=colors.green())
-		e.set_author(name='2B2TPE - Minecraft Anarchy', icon_url='https://cdn.discordapp.com/avatars/451107694934360094/b9b03963ff0eb72a1160412f2e2bfea4.webp?size=1024')
-		e.set_thumbnail(url='https://cdn.discordapp.com/icons/523678393565315077/1224c92145828542ac266dcf8804f70d.webp?size=1024')
-		e.description = '2B2TPE.ORG is an anarchy server based on 2b2t.org. At the moment it has the largest map when ranked with the other servers, ' \
-		    '(over 200gb), and one of the longest running maps of all bedrock anarchy servers, second only to 2p2e\n\n__For IOS, Android, and Windows 10:__\n**IP:** `2b2tPE.org` **Port:** `19132`\n' \
-		    '__For IOS, Android, Windows10, and Java:__\n**IP:** `2b2tPE.org` **Port:** `19153`\n' \
-		    '**Discord:** [click here](https://discord.gg/Ha5wxwd)'
+	def rotten_union(self):
+		e = discord.Embed(color=colors.purple())
+		e.set_author(name='Rotten Union', icon_url='https://cdn.discordapp.com/avatars/395228090013450241/f1f9803fa97805133dd18fc32e798ea1.webp?size=1024')
+		e.set_thumbnail(url='https://cdn.discordapp.com/attachments/642452511940280341/642460088312791060/662c684f2a983df4f85f.gif')
+		e.description = "Hello, and welcome to the up and coming TF2 community servers:\n" \
+						"Rotten Servers.\nCurrently we don't own any servers, but of course the more the merrier.\n" \
+		                "-giveaways\n-plenty of roles, and possibly more if you tell us to add some\n" \
+						"-mee6 ranking for that satisfying achievement feeling\n" \
+						"-healthy and friendly community\n" \
+						"-space to help you grow into your dreams\n" \
+						"-constantly online admins for your safety\n" \
+						"-always ready to hear your suggestions and to adapt to your liking\n" \
+						"e hope you, by joining, will help us grow to a massive and well known community, happiness is not something we lack.\n" \
+		                "Discord: [click here to join](https://discord.gg/PSc259G)"
+		e.set_image(url='https://cdn.discordapp.com/attachments/642452511940280341/642458545316691989/images.png')
 		return e
 
-	def mars(self):
-		e = discord.Embed(color=colors.orange())
-		e.set_author(name='Mars', icon_url='https://cdn.discordapp.com/avatars/544911653058248734/a_12ff164baa36ae2171358e968d148f4f.gif?size=1024')
-		e.set_thumbnail(url='https://cdn.discordapp.com/icons/610638435711189002/a_4841a26e78005fcf7b36974d0ab7d3eb.gif?size=1024')
-		e.description = 'Come land on Mars, where you can hang out with other Mars enthusiasts and talk games, pets, bots, or anything else, and even start your own fan club!'
-		return e
+
+
+	#def luke(self):
+	#	e = discord.Embed(color=colors.green())
+	#	e.set_author(name='2B2TPE - Minecraft Anarchy', icon_url='https://cdn.discordapp.com/avatars/451107694934360094/b9b03963ff0eb72a1160412f2e2bfea4.webp?size=1024')
+	#	e.set_thumbnail(url='https://cdn.discordapp.com/icons/523678393565315077/1224c92145828542ac266dcf8804f70d.webp?size=1024')
+	#	e.description = '2B2TPE.ORG is an anarchy server based on 2b2t.org. At the moment it has the largest map when ranked with the other servers, ' \
+	#	    '(over 200gb), and one of the longest running maps of all bedrock anarchy servers, second only to 2p2e\n\n__For IOS, Android, and Windows 10:__\n**IP:** `2b2tPE.org` **Port:** `19132`\n' \
+	#	    '__For IOS, Android, Windows10, and Java:__\n**IP:** `2b2tPE.org` **Port:** `19153`\n' \
+	#	    '**Discord:** [click here](https://discord.gg/Ha5wxwd)'
+	#	return e
+
+	#def mars(self):
+	#	e = discord.Embed(color=colors.orange())
+	#	e.set_author(name='Mars', icon_url='https://cdn.discordapp.com/avatars/544911653058248734/a_12ff164baa36ae2171358e968d148f4f.gif?size=1024')
+	#	e.set_thumbnail(url='https://cdn.discordapp.com/icons/610638435711189002/a_4841a26e78005fcf7b36974d0ab7d3eb.gif?size=1024')
+	#	e.description = 'Come land on Mars, where you can hang out with other Mars enthusiasts and talk games, pets, bots, or anything else, and even start your own fan club!'
+	#	return e
 
 class Polis(commands.Cog):
 	def __init__(self, bot: commands.Bot):
