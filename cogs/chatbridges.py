3828837dbf33d88d134a47db83eb1432d8e2242a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-12-01 21:29:47 -0500

    ignoring muted users still bRoKe

--- cogs/chatbridges.py
+++ cogs/chatbridges.py
@@ -130,6 +130,10 @@ class ChatBridge(commands.Cog):
 		""" Send the msg to the proper location """
 		if isinstance(msg.guild, discord.Guild) and self.category and not str(msg.author).endswith('#0000'):
 
+			for role in msg.author.roles:
+				if 'mute' in role.name.lower():
+					return
+
 			# anti spam
 			author_id = str(msg.author.id)
 			now = int(time() / 5)
@@ -172,9 +176,6 @@ class ChatBridge(commands.Cog):
 				for entry in bans:
 					if entry.user.id == msg.author.id:
 						return await msg.delete()
-				for role in msg.author.roles:
-					if 'muted' in role.name.lower():
-						return
 
 				# create new webhook if the old doesn't exits
 				webhooks = await channel.webhooks()
