31b60259e9aac4e1c32763a70b1504941a0b2832
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-12-28 00:30:18 -0500

    added the foundation for xp system that won't kill itself

---
+++ cogs/dev/ranking.py
@@ -0,0 +1,49 @@
+"""
+
+"""
+
+from os import path
+import os
+import json
+from time import time, monotonic
+from random import *
+import asyncio
+
+from discord.ext import commands
+import discord
+
+
+class Ranking(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.path = './static/xp.json'
+		self.globals = [
+			'msg', 'monthly_msg', 'vc', 'monthly_vc'
+		]
+
+		if not path.isdir('xp'):
+			os.mkdir('xp')
+			for filename in self.globals:
+				with open(path.join('xp', filename) + '.json', 'w') as f:
+					json.dump({}, f, ensure_ascii=False)
+
+		self.msg_cooldown = 10
+		self.cd = {}
+		self.macro_cd = {}
+		self.config = {}
+		if path.isfile(self.path):
+			with open(self.path, 'r') as f:
+				self.config = json.load(f)
+
+	def _global(self) -> dict:
+		""" Returns data for each global leaderboard """
+		dicts = {}
+		for filename in self.globals:
+			with open(path.join('xp', filename) + '.json', 'r') as f:
+				dicts[filename] = json.load(f)
+		return dicts
+
+
+
+def setup(bot):
+	bot.add_cog(Ranking(bot))
