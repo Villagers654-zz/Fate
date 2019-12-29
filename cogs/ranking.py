35644a986b795c397559e1778e4aec2d327116b9
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2019-12-28 19:10:37 -0500

    changed up the directorys for saving xp

--- cogs/ranking.py
+++ cogs/ranking.py
@@ -23,8 +23,10 @@ class Ranking(commands.Cog):
 
 		if not path.exists('xp'):
 			os.mkdir('xp')
+			os.mkdir(path.join('xp', 'global'))
+			os.mkdir(path.join('xp', 'guilds'))
 			for filename in self.globals:
-				with open(path.join('xp', filename) + '.json', 'w') as f:
+				with open(path.join('xp', 'global', filename) + '.json', 'w') as f:
 					json.dump({}, f, ensure_ascii=False)
 
 		self.msg = self._global('msg')
@@ -33,15 +35,17 @@ class Ranking(commands.Cog):
 		self.monthly_vc = self._global('monthly_vc')
 
 		self.guilds = {}
-		for directory in os.listdir('xp'):
+		for directory in os.listdir(path.join('xp', 'guilds')):
 			if directory.isdigit():
 				self.guilds[directory] = {}
-				for filename in os.listdir(path.join('xp', directory)):
-					with open(path.join('xp', directory, filename), 'r') as f:
-						self.guilds[directory][filename.replace('.json', '')] = json.load(f)
+				for filename in os.listdir(path.join('xp', 'guilds', directory)):
+					if '.json' in filename:
+						with open(path.join('xp', 'guilds', directory, filename), 'r') as f:
+							self.guilds[directory][filename.replace('.json', '')] = json.load(f)
 
 		self.msg_cooldown = 10
 		self.cd = {}
+		self.global_cd = {}
 		self.macro_cd = {}
 		self.counter = 0
 		self.backup_counter = 0
@@ -52,7 +56,7 @@ class Ranking(commands.Cog):
 
 	def _global(self, Global) -> dict:
 		""" Returns data for each global leaderboard """
-		with open(path.join('xp', Global) + '.json', 'r') as f:
+		with open(path.join('xp','global', Global) + '.json', 'r') as f:
 			return json.load(f)
 
 	def save_config(self):
@@ -80,23 +84,41 @@ class Ranking(commands.Cog):
 		if msg.guild and not msg.author.bot:
 			guild_id = str(msg.guild.id)
 			user_id = str(msg.author.id)
-			guild_path = path.join('xp', guild_id)
+			guild_path = path.join('xp', 'guilds', guild_id)
 
 			conf = self.static_config()  # type: dict
 			if guild_id in self.config:
 				conf = self.config[guild_id]
+			xp = randint(conf['min_xp_per_msg'], conf['max_xp_per_msg'])
 
-			if guild_id not in self.cd:
-				self.cd[guild_id] = {}
-			if user_id not in self.cd[guild_id]:
-				self.cd[guild_id][user_id] = []
-			msgs = [x for x in self.cd[guild_id][user_id] if float(x) > time() - conf['timeframe']]
-			if len(msgs) < conf['msgs_within_timeframe']:
-
+			# global leveling
+			if user_id not in self.global_cd:
+				self.global_cd[user_id] = 0
+			if self.global_cd[user_id] < time() - 10:
 				if user_id not in self.msg:
 					self.msg[user_id] = 0
 				if user_id not in self.monthly_msg:
 					self.monthly_msg[user_id] = {}
+
+				self.msg[user_id] += xp
+				self.monthly_msg[user_id][str(time())] = xp
+
+				self.counter += 1
+				if self.counter >= 10:
+					with open(path.join('xp', 'global', 'msg.json'), 'w') as f:
+						json.dump(self.msg, f, ensure_ascii=False)
+					with open(path.join('xp', 'global', 'monthly_msg.json'), 'w') as f:
+						json.dump(self.monthly_msg, f, ensure_ascii=True)
+					self.counter = 0
+
+			# per-server leveling
+			if guild_id not in self.cd:
+				self.cd[guild_id] = {}
+			if user_id not in self.cd[guild_id]:
+				self.cd[guild_id][user_id] = []
+			msgs = [x for x in self.cd[guild_id][user_id] if x > time() - conf['timeframe']]
+			if len(msgs) < conf['msgs_within_timeframe']:
+				self.cd[guild_id][user_id].append(time())
 				if not path.isdir(guild_path):
 					os.mkdir(guild_path)
 					for filename in self.globals:
@@ -110,9 +132,6 @@ class Ranking(commands.Cog):
 				if user_id not in self.guilds[guild_id]['monthly_msg']:
 					self.guilds[guild_id]['monthly_msg'][user_id] = {}
 
-				xp = randint(conf['min_xp_per_msg'], conf['max_xp_per_msg'])
-				self.msg[user_id] += xp
-				self.monthly_msg[user_id][str(time())] = xp
 				self.guilds[guild_id]['msg'][user_id] += xp
 				self.guilds[guild_id]['monthly_msg'][user_id][str(time())] = xp
 
@@ -121,21 +140,13 @@ class Ranking(commands.Cog):
 				with open(path.join(guild_path, 'monthly_msg.json'), 'w') as f:
 					json.dump(self.guilds[guild_id]['monthly_msg'], f, ensure_ascii=False)
 
-				self.counter += 1
-				if self.counter >= 10:
-					with open(path.join('xp', 'msg.json'), 'w') as f:
-						json.dump(self.msg, f, ensure_ascii=False)
-					with open(path.join('xp', 'msg.json'), 'w') as f:
-						json.dump(self.monthly_msg, f, ensure_ascii=True)
-					self.counter = 0
-
 				self.backup_counter += 1
 				if self.backup_counter > 25:
 					if not path.exists(path.join(guild_path, 'backup')):
 						os.mkdir(path.join(guild_path, 'backup'))
 					for filename in os.listdir(guild_path):
 						if '.' in filename:
-							with open(path.join(guild_path, 'filename'), 'r') as rf:
+							with open(path.join(guild_path, filename), 'r') as rf:
 								with open(path.join(guild_path, 'backup', filename), 'w') as wf:
 									wf.write(rf.read())
 					self.backup_counter = 0
@@ -147,9 +158,9 @@ class Ranking(commands.Cog):
 		e.set_thumbnail(url=self.bot.user.avatar_url)
 		e.description = ''
 		rank = 1
-		for user_id, xp in sorted(self.msg, key=lambda kv: kv[1], reverse=True)[:15]:
+		for user_id, xp in sorted(self.msg.items(), key=lambda kv: kv[1], reverse=True)[:15]:
 			user = await self.bot.fetch_user(int(user_id))
-			e.description += f"**#{rank}.** `{user.name}` - {xp}"
+			e.description += f"**#{rank}.** `{user.name}` - {xp}\n"
 			rank += 1
 		await ctx.send(embed=e)
 
