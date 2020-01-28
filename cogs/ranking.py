2294be19fc9828f06996e6458da9186a4febbb6d
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-01-27 23:24:09 -0500

    converted msg-xp gain and rank cards to sql

--- cogs/ranking.py
+++ cogs/ranking.py
@@ -9,6 +9,7 @@ import asyncio
 from datetime import datetime, timedelta
 import requests
 from io import BytesIO
+import aiosqlite
 
 from discord.ext import commands
 import discord
@@ -34,38 +35,9 @@ def profile_help():
 class Ranking(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.db_path = './data/userdata/xp.db'
 		self.path = './data/userdata/xp.json'
 		self.profile_path = './data/userdata/profiles.json'
-		self.log_path = './xp/msg_id_log.json'
-		self.globals = [
-			'msg', 'monthly_msg', 'vc'
-		]
-
-		if not path.exists('xp'):
-			os.mkdir('xp')
-			os.mkdir(path.join('xp', 'global'))
-			os.mkdir(path.join('xp', 'guilds'))
-			for filename in self.globals:
-				with open(path.join('xp', 'global', filename) + '.json', 'w') as f:
-					json.dump({}, f, ensure_ascii=False)
-
-		self.msg = self._global('msg')
-		self.monthly_msg = self._global('monthly_msg')
-		self.vc = self._global('vc')
-		self.gvclb = self._global('vc')
-
-		self.guilds = {}
-		for directory in os.listdir(path.join('xp', 'guilds')):
-			if directory.isdigit():
-				self.guilds[directory] = {}
-				for filename in os.listdir(path.join('xp', 'guilds', directory)):
-					if '.json' in filename:
-						try:
-							with open(path.join('xp', 'guilds', directory, filename), 'r') as f:
-								self.guilds[directory][filename.replace('.json', '')] = json.load(f)
-						except json.JSONDecodeError:
-							with open(path.join('xp', 'guilds', directory, 'backup', filename), 'r') as f:
-								self.guilds[directory][filename.replace('.json', '')] = json.load(f)
 
 		self.msg_cooldown = 10
 		self.cd = {}
@@ -90,14 +62,14 @@ class Ranking(commands.Cog):
 			with open(self.profile_path, 'r') as f:
 				self.profile = json.load(f)
 
-	def _global(self, Global) -> dict:
-		""" Returns data for each global leaderboard """
-		try:
-			with open(path.join('xp', 'global', Global) + '.json', 'r') as f:
-				return json.load(f)
-		except json.JSONDecodeError:
-			with open(path.join('xp', 'global', 'backup', Global + '.json'), 'r') as f:
-				return json.load(f)
+	# def _global(self, Global) -> dict:
+	# 	""" Returns data for each global leaderboard """
+	# 	try:
+	# 		with open(path.join('xp', 'global', Global) + '.json', 'r') as f:
+	# 			return json.load(f)
+	# 	except json.JSONDecodeError:
+	# 		with open(path.join('xp', 'global', 'backup', Global + '.json'), 'r') as f:
+	# 			return json.load(f)
 
 	def save_config(self):
 		""" Saves per-server configuration """
@@ -149,40 +121,45 @@ class Ranking(commands.Cog):
 			'xp': round(progress)
 		}
 
-
 	@commands.Cog.listener()
 	async def on_message(self, msg):
 		if msg.guild and not msg.author.bot:
 			guild_id = str(msg.guild.id)
-			user_id = str(msg.author.id)
-			guild_path = path.join('xp', 'guilds', guild_id)
+			user_id = msg.author.id
 
 			conf = self.static_config()  # type: dict
 			if guild_id in self.config:
 				conf = self.config[guild_id]
-			xp = randint(conf['min_xp_per_msg'], conf['max_xp_per_msg'])
+			new_xp = randint(conf['min_xp_per_msg'], conf['max_xp_per_msg'])
 
 			# global leveling
 			if user_id not in self.global_cd:
 				self.global_cd[user_id] = 0
 			if self.global_cd[user_id] < time() - 10:
-				if user_id not in self.msg:
-					self.msg[user_id] = 0
-				if user_id not in self.monthly_msg:
-					self.monthly_msg[user_id] = {}
-
-				self.msg[user_id] += 1
-				self.monthly_msg[user_id][str(time())] = 1
-
-				self.counter += 1
-				if self.counter >= 10:
-					with open(path.join('xp', 'global', 'msg.json'), 'w') as f:
-						json.dump(self.msg, f, ensure_ascii=False)
-					with open(path.join('xp', 'global', 'monthly_msg.json'), 'w') as f:
-						json.dump(self.monthly_msg, f, ensure_ascii=True)
-					self.counter = 0
+				async with aiosqlite.connect('./data/userdata/global-xp.db') as db:
+					await db.execute(f"PRAGMA journal_mode=WAL;")
+					cursor = await db.execute(f"SELECT xp FROM msg WHERE user_id = {user_id} LIMIT 1;")
+					dat = await cursor.fetchone()
+					if not dat:
+						await db.execute(f"INSERT INTO msg VALUES ({user_id}, 0);")
+						dat = (0,)
+					msg_xp = dat[0]  # type: int
+					cursor = await db.execute(f"SELECT * FROM monthly_msg WHERE user_id = {user_id};")
+					dat = await cursor.fetchall()
+					monthly_xp = 0
+					for user_id, msg_time, xp in dat:
+						if msg_time > time() - 60*60*24*30:
+							monthly_xp += msg_xp
+						else:
+							await db.execute(
+								f"DELETE FROM monthly_msg WHERE msg_time = '{msg_time}' and user_id = {user_id};"
+							)
+					await db.execute(f"UPDATE msg SET xp = {msg_xp+new_xp} WHERE user_id = {user_id};")
+					await db.execute(f"INSERT INTO monthly_msg VALUES ({user_id}, {time()}, {new_xp});")
+					await db.commit()
 
 			# per-server leveling
+			before = monotonic()
 			if guild_id not in self.cd:
 				self.cd[guild_id] = {}
 			if user_id not in self.cd[guild_id]:
@@ -190,134 +167,107 @@ class Ranking(commands.Cog):
 			msgs = [x for x in self.cd[guild_id][user_id] if x > time() - conf['timeframe']]
 			if len(msgs) < conf['msgs_within_timeframe']:
 				self.cd[guild_id][user_id].append(time())
-				if not path.isdir(guild_path):
-					os.mkdir(guild_path)
-					for filename in self.globals:
-						with open(path.join(guild_path, filename) + '.json', 'w') as f:
-							json.dump({}, f, ensure_ascii=False)
-					self.guilds[guild_id] = {
-						Global: {} for Global in self.globals
-					}
-				if user_id not in self.guilds[guild_id]['msg']:
-					self.guilds[guild_id]['msg'][user_id] = 0
-				if user_id not in self.guilds[guild_id]['monthly_msg']:
-					self.guilds[guild_id]['monthly_msg'][user_id] = {}
+				async with aiosqlite.connect('./data/userdata/msg-xp.db') as db:
+					await db.execute("PRAGMA journal_mode=WAL;")
+					await db.execute(f"CREATE TABLE IF NOT EXISTS '{guild_id}' (user_id int, xp int);")
+					cursor = await db.execute(f"SELECT xp FROM '{guild_id}' WHERE user_id = {user_id};")
+					dat = await cursor.fetchone()
+					if not dat:
+						await db.execute(f"INSERT INTO '{guild_id}' VALUES ({user_id}, 0);")
+						dat = (0,)
+					await db.execute(f"UPDATE '{guild_id}' SET xp = {new_xp+dat[0]} WHERE user_id = {user_id};")
+					await db.commit()
+				async with aiosqlite.connect('./data/userdata/monthly-xp.db') as db:
+					await db.execute(f"PRAGMA journal_mode=WAL;")
+					await db.execute(f"INSERT INTO '{guild_id}' VALUES ({user_id}, {time()}, {new_xp});")
+					await db.commit()
 
-				self.guilds[guild_id]['msg'][user_id] += xp
-				self.guilds[guild_id]['monthly_msg'][user_id][str(time())] = xp
-
-				with open(path.join(guild_path, 'msg.json'), 'w') as f:
-					json.dump(self.guilds[guild_id]['msg'], f, ensure_ascii=False)
-				with open(path.join(guild_path, 'monthly_msg.json'), 'w') as f:
-					json.dump(self.guilds[guild_id]['monthly_msg'], f, ensure_ascii=False)
-
-				self.backup_counter += 1
-				if self.backup_counter > 25:
-					# per-guild backup
-					if not path.exists(path.join(guild_path, 'backup')):
-						os.mkdir(path.join(guild_path, 'backup'))
-					for filename in os.listdir(guild_path):
-						if '.' in filename:
-							with open(path.join(guild_path, filename), 'r') as rf:
-								with open(path.join(guild_path, 'backup', filename), 'w') as wf:
-									wf.write(rf.read())
-					# global backup
-					backup_path = path.join('xp', 'global', 'backup')
-					if not path.exists(backup_path):
-						os.mkdir(backup_path)
-					for filename in os.listdir(path.join('xp', 'global')):
-						if '.' in filename:
-							with open(path.join('xp', 'global', filename), 'r') as rf:
-								with open(path.join(backup_path, filename), 'w') as wf:
-									wf.write(rf.read())
-					self.backup_counter = 0
-
-	@commands.Cog.listener()
-	async def on_voice_state_update(self, user, before, after):
-		if isinstance(user.guild, discord.Guild):
-			guild_id = str(user.guild.id)
-			channel_id = None  # type: discord.TextChannel
-			if before.channel:
-				channel_id = str(before.channel.id)
-			if after.channel:
-				channel_id = str(after.channel.id)
-			user_id = str(user.id)
-			guild_path = path.join('xp', 'guilds', guild_id)
-			if guild_id not in self.guilds:
-				self.guilds[guild_id] = {
-					Global: {} for Global in self.globals
-				}
-				os.mkdir(guild_path)
-				for filename in self.globals:
-					with open(path.join(guild_path, filename + '.json'), 'w') as f:
-						json.dump({}, f)
-			if user_id not in self.guilds[guild_id]['vc']:
-				self.guilds[guild_id]['vc'][user_id] = 0
-			if user_id not in self.gvclb:
-				self.gvclb[user_id] = 0
-			if channel_id not in self.cache:
-				self.cache[channel_id] = {}
-				self.cache[channel_id]['members'] = {}
-			def get_active_members(channel):
-				members = []
-				total = 0
-				for member in channel.members:
-					if not member.bot:
-						total += 1
-						state = member.voice
-						if not state.mute and not state.self_mute:
-							if not state.deaf and not state.self_deaf:
-								members.append(member)
-				return (members, total)
-			async def wrap(channel):
-				cid = str(channel.id)
-				for member_id in list(self.cache[cid]['members'].keys()):
-					seconds = (datetime.now() - self.cache[cid]['members'][member_id]).seconds
-					self.guilds[guild_id]['vc'][member_id] += seconds
-					self.gvclb[member_id] += seconds
-					del self.cache[cid]['members'][member_id]
-					save()
-			async def run(channel):
-				channel_id = str(channel.id)
-				members, total = get_active_members(channel)
-				if len(members) == 0 or len(members) == 1 and len(members) == total:
-					return await wrap(channel)
-				for member in channel.members:
-					if member not in self.cache[channel_id]['members']:
-						if not member.bot:
-							member_id = str(member.id)
-							if member_id not in self.cache[channel_id]['members']:
-								self.cache[channel_id]['members'][member_id] = datetime.now()
-			def save():
-				with open(path.join('xp', 'guilds', guild_id, 'vc.json'), 'w') as f:
-					json.dump(self.guilds[guild_id]['vc'], f)
-				with open(path.join('xp', 'global', 'vc.json'), 'w') as f:
-					json.dump(self.gvclb, f)
-				self.vc_counter = 0
-			if before.channel and after.channel:
-				if before.channel.id != after.channel.id:
-					channel_id = str(before.channel.id)
-					if user_id in self.cache[channel_id]['members']:
-						seconds = (datetime.now() - self.cache[channel_id]['members'][user_id]).seconds
-						self.guilds[guild_id]['vc'][user_id] += seconds
-						self.gvclb[user_id] += seconds
-						del self.cache[channel_id]['members'][user_id]
-						save()
-					await run(before.channel)
-					await run(after.channel)
-			if not after.channel:
-				channel_id = str(before.channel.id)
-				if user_id in self.cache[channel_id]['members']:
-					seconds = (datetime.now() - self.cache[channel_id]['members'][user_id]).seconds
-					self.guilds[guild_id]['vc'][user_id] += seconds
-					self.gvclb[user_id] += seconds
-					del self.cache[channel_id]['members'][user_id]
-					save()
-					await run(before.channel)
-			if before.channel is not None:
-				await run(before.channel)
-			if after.channel is not None:
-				await run(after.channel)
+	# @commands.Cog.listener()
+	# async def on_voice_state_update(self, user, before, after):
+	# 	if isinstance(user.guild, discord.Guild):
+	# 		guild_id = str(user.guild.id)
+	# 		channel_id = None  # type: discord.TextChannel
+	# 		if before.channel:
+	# 			channel_id = str(before.channel.id)
+	# 		if after.channel:
+	# 			channel_id = str(after.channel.id)
+	# 		user_id = str(user.id)
+	# 		guild_path = path.join('xp', 'guilds', guild_id)
+	# 		if guild_id not in self.guilds:
+	# 			self.guilds[guild_id] = {
+	# 				Global: {} for Global in self.globals
+	# 			}
+	# 			os.mkdir(guild_path)
+	# 			for filename in self.globals:
+	# 				with open(path.join(guild_path, filename + '.json'), 'w') as f:
+	# 					json.dump({}, f)
+	# 		if user_id not in self.guilds[guild_id]['vc']:
+	# 			self.guilds[guild_id]['vc'][user_id] = 0
+	# 		if user_id not in self.gvclb:
+	# 			self.gvclb[user_id] = 0
+	# 		if channel_id not in self.cache:
+	# 			self.cache[channel_id] = {}
+	# 			self.cache[channel_id]['members'] = {}
+	# 		def get_active_members(channel):
+	# 			members = []
+	# 			total = 0
+	# 			for member in channel.members:
+	# 				if not member.bot:
+	# 					total += 1
+	# 					state = member.voice
+	# 					if not state.mute and not state.self_mute:
+	# 						if not state.deaf and not state.self_deaf:
+	# 							members.append(member)
+	# 			return (members, total)
+	# 		async def wrap(channel):
+	# 			cid = str(channel.id)
+	# 			for member_id in list(self.cache[cid]['members'].keys()):
+	# 				seconds = (datetime.now() - self.cache[cid]['members'][member_id]).seconds
+	# 				self.guilds[guild_id]['vc'][member_id] += seconds
+	# 				self.gvclb[member_id] += seconds
+	# 				del self.cache[cid]['members'][member_id]
+	# 				save()
+	# 		async def run(channel):
+	# 			channel_id = str(channel.id)
+	# 			members, total = get_active_members(channel)
+	# 			if len(members) == 0 or len(members) == 1 and len(members) == total:
+	# 				return await wrap(channel)
+	# 			for member in channel.members:
+	# 				if member not in self.cache[channel_id]['members']:
+	# 					if not member.bot:
+	# 						member_id = str(member.id)
+	# 						if member_id not in self.cache[channel_id]['members']:
+	# 							self.cache[channel_id]['members'][member_id] = datetime.now()
+	# 		def save():
+	# 			with open(path.join('xp', 'guilds', guild_id, 'vc.json'), 'w') as f:
+	# 				json.dump(self.guilds[guild_id]['vc'], f)
+	# 			with open(path.join('xp', 'global', 'vc.json'), 'w') as f:
+	# 				json.dump(self.gvclb, f)
+	# 			self.vc_counter = 0
+	# 		if before.channel and after.channel:
+	# 			if before.channel.id != after.channel.id:
+	# 				channel_id = str(before.channel.id)
+	# 				if user_id in self.cache[channel_id]['members']:
+	# 					seconds = (datetime.now() - self.cache[channel_id]['members'][user_id]).seconds
+	# 					self.guilds[guild_id]['vc'][user_id] += seconds
+	# 					self.gvclb[user_id] += seconds
+	# 					del self.cache[channel_id]['members'][user_id]
+	# 					save()
+	# 				await run(before.channel)
+	# 				await run(after.channel)
+	# 		if not after.channel:
+	# 			channel_id = str(before.channel.id)
+	# 			if user_id in self.cache[channel_id]['members']:
+	# 				seconds = (datetime.now() - self.cache[channel_id]['members'][user_id]).seconds
+	# 				self.guilds[guild_id]['vc'][user_id] += seconds
+	# 				self.gvclb[user_id] += seconds
+	# 				del self.cache[channel_id]['members'][user_id]
+	# 				save()
+	# 				await run(before.channel)
+	# 		if before.channel is not None:
+	# 			await run(before.channel)
+	# 		if after.channel is not None:
+	# 			await run(after.channel)
 
 	@commands.command(name='xp-config')
 	@commands.cooldown(*utils.default_cooldown())
@@ -502,22 +452,41 @@ class Ranking(commands.Cog):
 				background_url = self.profile[user_id]['background']
 
 		# xp variables
-		guild_rank = 0
-		for id, xp in (sorted(self.guilds[guild_id]['msg'].items(), key=lambda kv: kv[1], reverse=True)):
-			guild_rank += 1
-			if user_id == id:
-				break
+		async with aiosqlite.connect('./data/userdata/msg-xp.db') as db:
+			cursor = await db.execute(f"SELECT * FROM '{guild_id}' ORDER BY xp DESC;")
+			guild_xp = await cursor.fetchall()
+			guild_rank = 0
+			for i, (id, xp) in enumerate(guild_xp):
+				if id == user.id:
+					guild_rank = i + 1
+					break
+			cursor = await db.execute(f"SELECT xp FROM '{guild_id}' WHERE user_id = '{user.id}';")
+			guild_xp = await cursor.fetchone()
 		conf = self.config[guild_id]
 		if 'global' in ctx.message.content:
-			dat = self.calc_lvl(self.msg[user_id], self.static_config())
+			async with aiosqlite.connect('./data/userdata/global-xp.db') as db:
+				cursor = await db.execute(f"SELECT * FROM msg ORDER BY xp DESC;")
+				global_xp = await cursor.fetchall()
+				guild_rank = 0
+				for i, (id, xp) in enumerate(global_xp):
+					if id == user.id:
+						guild_rank = i + 1
+						break
+				cursor = await db.execute(f"SELECT xp FROM msg WHERE user_id = '{user.id}';")
+				guild_xp = await cursor.fetchone()
+			if not guild_xp:
+				return await ctx.send('somehow I have no xp for you .-.')
+			dat = self.calc_lvl(guild_xp[0], self.static_config())
 		else:
-			dat = self.calc_lvl(self.guilds[guild_id]['msg'][user_id], conf)
+			if not guild_xp:
+				return await ctx.send('somehow I have no xp for this server .-.')
+			dat = self.calc_lvl(guild_xp[0], conf)
 		base_req = self.config[guild_id]['first_lvl_xp_req']
 		level = dat['level']
 		xp = dat['xp']
 		max_xp = base_req if level == 0 else dat['level_up']
 		length = ((100 * (xp / max_xp)) * 1000) / 100
-		total = f'Total: {self.guilds[guild_id]["msg"][user_id]}'
+		total = f'Total: {guild_xp[0]}'
 		required = f'Required: {max_xp - xp}'
 		progress = f'{xp} / {max_xp} xp'
 		misc = f'{progress} | {total} | {required}'
@@ -716,6 +685,8 @@ class Ranking(commands.Cog):
 			embeds.append(e)
 
 			return embeds
+
+		return await ctx.send("Ranking is currently undergoing changes, you'll still receive xp from messages")
 
 		with open('./data/config.json', 'r') as f:
 			config = json.load(f)  # type: dict
