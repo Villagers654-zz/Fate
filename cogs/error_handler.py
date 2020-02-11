68203347bff0f9034ebce529fc16855c7fc11cb3
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-02-10 20:39:00 -0500

    sends check failure errors again

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -39,9 +39,7 @@ class ErrorHandler(commands.Cog):
 		elif isinstance(error, commands.MissingRequiredArgument):
 			return await ctx.send(error)
 		elif isinstance(error, commands.CheckFailure):
-			await ctx.message.add_reaction('✋')
-			await ctx.message.add_reaction('✊')
-			await ctx.message.add_reaction('😤')
+			await ctx.send(error)
 			return await ctx.message.add_reaction('🚫')
 		elif isinstance(error, discord.errors.Forbidden):
 			bot = ctx.guild.get_member(self.bot.user.id)
