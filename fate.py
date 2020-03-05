afb52ea057e9c24f24f579b000e4da7a375d3193
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-03-05 15:12:04 -0500

    update initial extensions

--- fate.py
+++ fate.py
@@ -42,7 +42,7 @@ initial_extensions = [
 	'lock', 'audit', 'cookies', 'backup', 'stats', 'server_list', 'emojis', 'logger', 'autorole', 'changelog',
 	'restore_roles', 'chatbot', 'anti_spam', 'anti_raid', 'chatfilter', 'nsfw', 'minecraft', 'chatlock', 'rainbow',
 	'system', 'user', 'limiter', 'dm_channel', 'factions', 'secure_overwrites', 'server_setup', 'secure-log',
-	'global-chat', 'beta'
+	'global-chat', 'beta', 'ranking'
 ]
 login_errors = []
 
