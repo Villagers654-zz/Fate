f3ca8cc2ef9213092053b07cc1b8f7449995e9d2
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-07-16 03:14:33 -0400

    add bulk msg handler to verification channels and add the disable command

--- cogs/verification.py
+++ cogs/verification.py
@@ -16,7 +16,6 @@ from utils import utils, colors
 
 
 class Verification(commands.Cog):
-
     def __init__(self, bot: Fate):
         self.bot = bot
         self.path = "./data/userdata/verification.json"
@@ -24,16 +23,15 @@ class Verification(commands.Cog):
         if path.isfile(self.path):
             with open(self.path, "r") as f:
                 self.config = json.load(f)
+        self.queue = {}
+        self.running_tasks = []
 
     @property
     def template_config(self):
         return {
-            "channel_id":
-                int,  # Verification channel, required incase the bot can't dm
-            "verified_role_id":
-                int,  # Role to give whence verified
-            "temp_role_id":
-                Optional[None, int]  # Role to remove whence verified
+            "channel_id": int,  # Verification channel, required incase the bot can't dm
+            "verified_role_id": int,  # Role to give whence verified
+            "temp_role_id": Optional[None, int]  # Role to remove whence verified
         }
 
     async def save_data(self):
@@ -54,11 +52,11 @@ class Verification(commands.Cog):
                 f"\n`start a simple and guided setup process`"
                 f"{p}verification disable"
                 f"\n`wipes your current configuration`"
-                f"\n{p}verification set-verified-role @role"
+                f"\n~~{p}verification set-verified-role @role"
                 f"\n`change the role given whence verified`"
                 f"\n{p}verification set-temp-role @role"
                 f"\n`set or change the role to remove whence verified. this is an optional feature, "
-                f"and isn't required in order for verification to work`",
+                f"and isn't required in order for verification to work`~~",
                 inline=False)
             guild_id = str(ctx.guild.id)
             if guild_id in self.config:
@@ -85,18 +83,48 @@ class Verification(commands.Cog):
                     }))
             await ctx.send(embed=e)
 
-    async def bulk_purge(self, collection_period: int = 5):
-        pass
+    @verification.group(name="disable")
+    @commands.has_permissions(administrator=True)
+    async def _disable(self, ctx):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.config:
+            return await ctx.send("Verification isn't enabled")
+        del self.config[guild_id]
+        await ctx.send("Disabled verification")
+        await self.save_data()
+
+    async def bulk_purge(self, channel: discord.TextChannel, collection_period: int = 5):
+        guild_id = str(channel.guild.id)
+        await asyncio.sleep(collection_period)
+        target_messages = [msg for msg in self.queue[guild_id] if msg]
+        for message in target_messages:
+            self.queue[guild_id].remove(message)
+        try:
+            await channel.delete_messages(target_messages)
+        except NotFound:
+            for msg in target_messages:
+                with suppress(NotFound):
+                    await msg.delete()
 
     @commands.Cog.listener("on_message")
     async def channel_cleanup(self, msg):
-        pass
+        guild_id = str(msg.guild.id)
+        if not msg.author.bot and guild_id in self.config:
+            if msg.channel.id == self.config[guild_id]["channel_id"]:
+                if guild_id not in self.queue:
+                    self.queue[guild_id] = []
+                self.queue[guild_id].append(msg)
+                if guild_id not in self.running_tasks:
+                    self.running_tasks.append(guild_id)
+                    with suppress(IndexError, NotFound, Forbidden, HTTPException):
+                        await self.bulk_purge(msg.channel, collection_period=5)
+                    self.running_tasks.remove(guild_id)
 
     @commands.Cog.listener("on_member_join")
     async def init_verification_process(self, member: discord.Member):
         guild_id = str(member.guild.id)
         if guild_id in self.config:
-            conf = self.config[guild_id]  # type: dict
+            conf = self.config[guild_id]  # type: Verification.template_config
             try:
                 channel = await self.bot.fetch_channel(conf["channel_id"])
             except (NotFound, HTTPException, Forbidden):
@@ -105,6 +133,7 @@ class Verification(commands.Cog):
                         f"Disabled verification in {member.guild} due to the channel being deleted"
                     )
                 del self.config[guild_id]
+                await self.save_data()
                 return
             verified_role = member.guild.get_role(conf["verified_role_id"])
             if not verified_role:
@@ -113,10 +142,12 @@ class Verification(commands.Cog):
                         f"Disabled verification in {member.guild} due to the verified role being deleted"
                     )
                 del self.config[guild_id]
+                await self.save_data()
                 return
             msg = await channel.send(
                 f"{member.mention} complete this captcha to be verified",
-                delete_after=45)
+                delete_after=45
+            )
             ctx = await self.bot.get_context(msg)
             verified = await self.bot.verify_user(ctx, user=member)
             if verified:
@@ -129,6 +160,7 @@ class Verification(commands.Cog):
                                 f"Disabled verification in {member.guild} due to the verified role being deleted"
                             )
                         self.config[guild_id]["temp_role_id"] = None
+                        await self.save_data()
                     else:
                         if temp_role in member.roles:
                             await member.remove_roles(temp_role)
