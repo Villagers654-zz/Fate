9fcf67f9c88a73b6a377b23d0a78a1b96365899a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-09-07 22:06:43 -0400

    add more options to .verification

--- cogs/verification.py
+++ cogs/verification.py
@@ -5,6 +5,7 @@ from os import path
 import json
 from typing import Optional
 from contextlib import suppress
+from datetime import datetime, timedelta
 
 from discord.ext import commands
 import discord
@@ -23,6 +24,13 @@ class Verification(commands.Cog):
         if path.isfile(self.path):
             with open(self.path, "r") as f:
                 self.config = json.load(f)
+        for guild_id, config in list(self.config.items()):
+            if "log_channel" not in config:
+                config["log_channel"] = None
+            if "kick_on_fail" not in config:
+                config["kick_on_fail"] = True
+            if "auto_start" not in config:
+                config["auto_start"] = True
         self.queue = {}
         self.running_tasks = []
 
@@ -32,7 +40,10 @@ class Verification(commands.Cog):
             "channel_id": int,                    # Verification channel, required incase the bot can't dm
             "verified_role_id": int,              # Role to give whence verified
             "temp_role_id": Optional[None, int],  # Role to remove whence verified
-            "delete_after": bool                  # Delete captcha message after users are verified
+            "delete_after": bool,                 # Delete captcha message after users are verified
+            "log_channel": int,
+            "kick_on_fail": bool,
+            "auto_start": bool
         }
 
     async def save_data(self):
@@ -52,17 +63,32 @@ class Verification(commands.Cog):
                 name="◈ Usage",
                 value=f"{p}verification enable"
                 f"\n`start a simple and guided setup process`"
+                      
                 f"\n{p}verification disable"
                 f"\n`wipes your current configuration`"
+                      
                 f"\n{p}verification set-channel #channel"
                 f"\n`changes the linked channel`"
+                      
                 f"\n{p}verification set-verified-role @role"
                 f"\n`change the role given whence verified`"
+                      
                 f"\n{p}verification set-temp-role @role"
                 f"\n`set or change the role to remove whence verified. this is an optional feature, "
                 f"and isn't required in order for verification to work`"
+                      
                 f"\n{p}verification delete-after"
-                f"\n`toggles whether or not to delete the captcha after a user completes verification`",
+                f"\n`toggles whether or not to delete the captcha after a user completes verification`"
+                      
+                f"\n{p}verification kick"
+                f"\n`toggles whether or not to kick after failing verification`"
+                      
+                f"\n{p}verification auto-start"
+                f"\n`toggles verification starting automatically when a new member joins instead of using .verify`"
+                      
+                f"\n{p}verification log-channel #channel"
+                f"\n`sets the channel to log whether people succeeded or failed verification. use this without "
+                f"the #channel argument to disable this feature`",
                 inline=False)
             guild_id = str(ctx.guild.id)
             if guild_id in self.config:
@@ -76,6 +102,10 @@ class Verification(commands.Cog):
                         temp_role = temp_role.mention
                     else:
                         temp_role = "deleted-role"
+                log_channel = None
+                if conf["log_channel"]:
+                    log_channel = self.bot.get_channel(conf["log_channel"])
+                log_channel = log_channel.mention if log_channel else "Not Set"
                 e.add_field(
                     name="◈ Current Configuration",
                     value=self.bot.utils.format_dict({
@@ -86,8 +116,14 @@ class Verification(commands.Cog):
                             if verified_role else 'deleted-role',
                         "Temp Role":
                             temp_role,
+                        "Log Channel":
+                            log_channel,
                         "Delete captcha after":
-                            str(conf["delete_after"])
+                            str(conf["delete_after"]),
+                        "Auto Start":
+                            str(conf["auto_start"]),
+                        "Kick on Fail":
+                            str(conf["kick_on_fail"])
                     }))
             await ctx.send(embed=e)
 
@@ -135,11 +171,42 @@ class Verification(commands.Cog):
                 delete_after = True
             else:
                 delete_after = False
+
+        await ctx.send("Should I kick members if they fail verification? Reply with `yes` or `no`")
+        async with self.bot.require('message', ctx) as msg:
+            if 'ye' not in str(msg.content).lower() and 'no' not in str(msg.content).lower():
+                return await ctx.send("Invalid response, please rerun the command")
+            elif 'ye' in str(msg.content).lower():
+                kick_on_fail = True
+            else:
+                kick_on_fail = False
+
+        await ctx.send("Now, should I log whether or not someone passes, fails, or kick to a channel? "
+                       "If so, #mention the channel I should use; otherwise send `skip`")
+        async with self.bot.require('message', ctx) as msg:
+            if msg.channel_mentions:
+                log_channel = msg.channel_mentions[0].id
+            else:
+                log_channel = None
+
+        await ctx.send("When a new member joins, should I start captcha verification on my own, or "
+                       "wait until they run .verify. Reply with `yes` to start automatically, or `no` to not")
+        async with self.bot.require("message", ctx) as msg:
+            if 'ye' not in str(msg.content).lower() and 'no' not in str(msg.content).lower():
+                return await ctx.send("Invalid response, please rerun the command")
+            elif 'ye' in str(msg.content).lower():
+                automatic = True
+            else:
+                automatic = False
+
         self.config[guild_id] = {
             "channel_id": channel.id,     # Verification channel, required incase the bot can't dm
             "verified_role_id": role.id,  # Role to give whence verified
             "temp_role_id": temp_role,    # Role to remove whence verified
-            "delete_after": delete_after
+            "delete_after": delete_after,
+            "kick_on_fail": kick_on_fail,
+            "log_channel": log_channel,
+            "auto_start": automatic
         }
         await ctx.send("Successfully setup the verification system")
         await self.save_data()
@@ -210,8 +277,54 @@ class Verification(commands.Cog):
         if toggle:
             new_toggle = toggle
         self.config[guild_id]["delete_after"] = new_toggle
-        await ctx.send(f"{'enabled' if new_toggle else 'disabled'} delete-after")
+        await ctx.send(f"{'Enabled' if new_toggle else 'Disabled'} delete-after")
         await self.save_data()
+
+    @verification.command(name="kick")
+    @commands.has_permissions(administrator=True)
+    async def _kick(self, ctx, toggle: Optional[bool]):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.config:
+            return await ctx.send("Verification isn't enabled")
+        if toggle:
+            new_toggle = toggle
+        else:
+            new_toggle = not self.config[guild_id]["kick_on_fail"]
+        self.config[guild_id]["kick_on_fail"] = new_toggle
+        await ctx.send(f"{'Enabled' if new_toggle else 'Disabled'} kicking on fail")
+        await self.save_data()
+
+    @verification.command(name="auto", aliases=["automatic"])
+    @commands.has_permissions(administrator=True)
+    async def auto_start(self, ctx, toggle: Optional[bool]):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.config:
+            return await ctx.send("Verification isn't enabled")
+        if toggle:
+            new_toggle = toggle
+        else:
+            new_toggle = not self.config[guild_id]["auto_start"]
+        self.config[guild_id]["auto_start"] = new_toggle
+        await ctx.send(f"{'Enabled' if new_toggle else 'Disabled'} automatically starting verification")
+        await self.save_data()
+
+    @verification.command(name="log-channel", aliases=["logchannel"])
+    @commands.has_permissions(administrator=True)
+    async def _log_channel(self, ctx, channel: Optional[discord.TextChannel]):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.config:
+            return await ctx.send("Verification isn't enabled")
+        self.config[guild_id]["log_channel"] = channel.id if channel else None
+        await ctx.send(f"{'Set' if channel else 'Removed'} the log channel")
+        await self.save_data()
+
+    @commands.command(name="verify")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def verify(self, ctx):
+        guild_id = str(ctx.guild.id)
+        if guild_id not in self.config:
+            return await ctx.send("Verification isn't enabled")
+        await self.init_verification_process(ctx.author)
 
     async def bulk_purge(self, channel: discord.TextChannel, collection_period: int = 5):
         """Collect messages for X seconds and bulk delete"""
@@ -252,6 +365,9 @@ class Verification(commands.Cog):
         guild_id = str(member.guild.id)
         if guild_id in self.config and not member.bot:
             conf = self.config[guild_id]  # type: Verification.template_config
+            if member.joined_at > datetime.utcnow() - timedelta(seconds=2):
+                if not conf["auto_start"]:
+                    return
             try:
                 channel = await self.bot.fetch_channel(conf["channel_id"])
             except (NotFound, HTTPException, Forbidden):
@@ -262,6 +378,19 @@ class Verification(commands.Cog):
                 del self.config[guild_id]
                 await self.save_data()
                 return
+            log_channel = None
+            if conf["log_channel"]:
+                try:
+                    log_channel = await self.bot.fetch_channel(conf["log_channel"])
+                except (NotFound, HTTPException, Forbidden):
+                    log_channel = None
+                    self.config[guild_id]["log_channel"] = None
+                    await self.save_data()
+            if conf["kick_on_fail"]:
+                bot = member.guild.me  # type: discord.Member
+                if not bot.guild_permissions.kick_members or member.top_role > bot.top_role:
+                    await channel.send("I'm missing kick permission(s) to properly manage verification")
+                    return
             verified_role = member.guild.get_role(conf["verified_role_id"])
             if not verified_role:
                 with suppress(Forbidden, HTTPException):
@@ -278,18 +407,20 @@ class Verification(commands.Cog):
                 await member.add_roles(verified_role)
                 if conf["temp_role_id"]:
                     temp_role = member.guild.get_role(conf["temp_role_id"])
-                    if not temp_role:
+                    if temp_role:
+                        if temp_role in member.roles:
+                            await member.remove_roles(temp_role)
+                    else:
                         with suppress(Forbidden, HTTPException):
                             await channel.send(
                                 f"Disabled verification in {member.guild} due to the verified role being deleted"
                             )
                         self.config[guild_id]["temp_role_id"] = None
                         await self.save_data()
-                    else:
-                        if temp_role in member.roles:
-                            await member.remove_roles(temp_role)
+                if log_channel:
+                    await log_channel.send(f"{member} was verified")
             else:
-                if verified_role not in member.roles:
+                if verified_role not in member.roles and conf["kick_on_fail"]:
                     try:
                         await member.kick(reason="Failed Captcha Verification")
                     except Forbidden:
@@ -297,6 +428,12 @@ class Verification(commands.Cog):
                             await member.guid.owner.send(
                                 f"I'm missing permissions to kick unverified members in {member.guild}"
                             )
+                    else:
+                        if log_channel:
+                            await log_channel.send(f"Kicked {member} for failing verification")
+                else:
+                    if log_channel:
+                        await log_channel.send(f"{member} failed to complete the captcha")
 
 
 def setup(bot):
