e4f57269f17d02383e3804bd642b23cb09cd7ceb
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-12-05 02:28:29 -0500

    finish .reply and added mod reply support to modmail.py

--- cogs/moderation/modmail.py
+++ cogs/moderation/modmail.py
@@ -1,9 +1,11 @@
 
 import json
 from time import time
+from contextlib import suppress
 
 from discord.ext import commands
 import discord
+from discord.errors import Forbidden
 
 
 class ModMail(commands.Cog):
@@ -104,30 +106,35 @@ class ModMail(commands.Cog):
         await ctx.send(f"Unblocked {user} from using modmail")
 
     @commands.command(name="reply")
-    @commands.dm_only()
-    async def reply(self, ctx, case_number: int = None):
+    async def reply(self, ctx, case_number: int = None, *, message=None):
         async with self.bot.cursor() as cur:
             if case_number:
                 await cur.execute(
                     f"select guild_id, case_number, reason, link, created_at from cases "
-                    f"where case_number = {case_number} and user_id = {ctx.author.id};"
+                    f"where case_number = {case_number} and user_id = {ctx.author.id}"
+                    f"limit 1;"
                 )
                 results = await cur.fetchall()
             else:
                 await cur.execute(
                     f"select guild_id, case_number, reason, link, created_at from cases "
                     f"where user_id = {ctx.author.id} "
-                    f"and created_at > {time() - 60 * 60 * 24 * 14};"
+                    f"and created_at > {time() - 60 * 60 * 24 * 14} "
+                    f"limit 1;"
                 )
                 results = await cur.fetchall()
+
         if not results:
             if case_number:
-                return await ctx.send("Couldn't find any cases for you with that case number")
+                await ctx.send("Couldn't find any cases for you with that case number")
+                return None
             p = self.bot.utils.get_prefix(ctx)  # type: str
-            return await ctx.send(
+            await ctx.send(
                 f"Couldn't find any cases from you from within the last 14 days. "
                 f"Use `{p}reply [case_number]` to specify which"
             )
+            return None
+
         sorted_results = sorted(results, key=lambda lst: lst[4])[:5]
         formatted_results = [
             f"[Case #{case} from {self.bot.get_guild(guild_id)}]({link})" \
@@ -136,14 +143,130 @@ class ModMail(commands.Cog):
         ]
         choice = await self.bot.utils.get_choice(ctx, *formatted_results, user=ctx.author)
         if not choice:
-            return await ctx.send("Timed out waiting for choice")
+            await ctx.send("Timed out waiting for choice")
+            return None
         result = sorted_results[formatted_results.index(choice)]
         guild_id, case, reason, link, created_at = result
-        await ctx.send(f"You chose case #{case} from {self.bot.get_guild(guild_id)}")
 
-    @commands.command(name="close")
-    async def close(self, ctx):
-        pass
+        await cur.execute(
+            f"select channel_id from modmail "
+            f"where guild_id = {guild_id} "
+            f"limit 1;"
+        )
+        results = await cur.fetchone()
+        if not results:
+            await ctx.send("Modmail isn't enabled in that server")
+            return None
+
+        category = self.bot.get_channel(results[0])
+        if not category:
+            await ctx.send("Couldn't get the modmail channel in that guild, sorry")
+            return None
+
+        thread_id = f"case-{case}"
+        no_content = not message and not ctx.message.attachments
+        if no_content and any(thread_id == channel.name for channel in category.channels):
+            return await ctx.send("There's already an active thread in that server")
+
+        matches = [channel for channel in category.channels if thread_id == channel.name]
+        if not matches:
+            try:
+                channel = await category.create_text_channel(name=thread_id)
+            except Forbidden:
+                await ctx.send("Failed to create a thread due to my lacking manage_channel perms in that server")
+                return None
+            e = discord.Embed(color=self.bot.config["theme_color"])
+            e.set_author(name=str(ctx.author), icon_url=ctx.author.avatar_url)
+            e.description = f"**Case #{case}:**\n"
+            if link:
+                e.description += f"Reason: [{reason}]({link})\n"
+            else:
+                e.description += f"Reason: `{reason}`"
+            try:
+                await channel.send(embed=e)
+            except Forbidden:
+                await ctx.send("Failed to create a thread due to my lacking manage_channel perms in that server")
+                return None
+            await ctx.send("Created your thread 👍")
+        else:
+            channel = matches[0]  # type: discord.TextChannel
+            e = discord.Embed(color=self.bot.config["theme_color"])
+            e.set_author(name="New Reply", icon_url=ctx.author.avatar_url)
+            if message:
+                e.description = message
+            if ctx.message.attachments:
+                e.set_image(url=ctx.message.attachments[0].url)
+            try:
+                await channel.send(embed=e)
+            except Forbidden:
+                await ctx.send("Failed to create a thread due to my lacking manage_channel perms in that server")
+                return None
+            await ctx.send("Replied to your thread 👍")
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if (msg.content or msg.attachments) and isinstance(msg.guild, discord.Guild) and "case-" in msg.channel.name:
+            name = msg.channel.name.replace("case-", "")
+            if name.isdigit() and int(name) in self.bot.modmail_threads:
+                case_number = int(name)
+                async with self.bot.cursor() as cur:
+                    await cur.execute(
+                        f"select user_id "
+                        f"from cases "
+                        f"where guild_id = {msg.guild.id} "
+                        f"and case_number = {case_number};"
+                    )
+                    result = await cur.fetchone()
+                    if result:
+                        user_id = result[0]
+                    else:
+                        return None
+                    user = self.bot.get_user(user_id)
+
+                    if ".close" in msg.content.lower():
+                        if msg.channel.permissions_for(msg.guild.me).read_messages:
+                            pass
+                            # history = await msg.channel.history(limit=75).flatten()
+                        if user:
+                            with suppress(Forbidden):
+                                await user.send(f"Case #{case_number} was closed in {msg.guild}")
+                        try:
+                            await msg.channel.delete()
+                        except:
+                            await msg.channel.send(
+                                f"To close the thread, delete the channel"
+                            )
+                        return None
+
+                    elif not user:
+                        await msg.channel.send(
+                            f"I no longer share any servers with this user, therefore cannot dm them"
+                        )
+                        return None
+
+                    e = discord.Embed(color=self.bot.config["theme_color"])
+                    e.set_author(name=f"Case #{case_number} in {msg.guild}", icon_url=msg.guild.icon_url)
+                    e.description = f"Reply from {msg.author}"
+                    if msg.content:
+                        e.add_field(
+                            name="◈ Message",
+                            value=msg.content[:1024],
+                            inline=False
+                        )
+                    if msg.attachments:
+                        e.set_image(url=msg.attachments[0].url)
+                    e.set_footer(text=f"Use .reply {case_number} [your message]")
+                    user = self.bot.get_user(user_id)
+                    if not user:
+                        await msg.channel.send(f"I no longer share any servers with this user, therefore cannot dm them")
+                        return None
+                    try:
+                        await user.send(embed=e)
+                    except Forbidden:
+                        await msg.channel.send(
+                            "Failed to reply to the user. Either their dms are closed "
+                            "or I no longer share any servers with them"
+                        )
 
 
 def setup(bot):
