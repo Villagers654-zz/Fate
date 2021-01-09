faed01258a04489113b0fe105ad8a104a8114f6b
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-01-08 22:12:57 -0500

    move parse_check outside of the CreateListener class

--- botutils/packages/listeners.py
+++ botutils/packages/listeners.py
@@ -8,27 +8,27 @@ class CheckError(Exception):
     pass
 
 
+def parse_check(check) -> int:
+    if hasattr(check, "__call__"):
+        return check
+    ids = []
+    if isinstance(check, Context):
+        ids.append(check.author.id)
+    elif isinstance(check, (User, Member, Message)):
+        ids.append(check.id)
+    elif isinstance(check, int):
+        ids.append(check)
+    else:
+        raise CheckError(f"Check of type '{type(check)}' isn't implemented")
+    return ids[0]
+
+
 class CreateListener:
     def __init__(self, bot):
         self.bot = bot
 
-    @staticmethod
-    def parse_check(check) -> int:
-        if hasattr(check, "__call__"):
-            return check
-        ids = []
-        if isinstance(check, Context):
-            ids.append(check.author.id)
-        elif isinstance(check, (User, Member, Message)):
-            ids.append(check.id)
-        elif isinstance(check, int):
-            ids.append(check)
-        else:
-            raise CheckError(f"Check of type '{type(check)}' isn't implemented")
-        return ids[0]
-
     async def get_reaction(self, check, timeout=60, ignore_timeout=True):
-        target_id = self.parse_check(check)  # type: int
+        target_id = parse_check(check)  # type: int
 
         def predicate(r, u):
             return r.message.id == target_id or u.id == target_id
@@ -48,7 +48,7 @@ class CreateListener:
         return reaction, user
 
     async def get_message(self, check, timeout=60, ignore_timeout=True):
-        target = self.parse_check(check)
+        target = parse_check(check)
 
         def predicate(m):
             return m.author.id == target
