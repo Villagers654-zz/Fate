bb4c521b78017ec950898ccd7b882c8d8c3bc2b1
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-02-16 19:32:29 -0500

    add/remove support to utils.get_reaction

--- botutils/packages/listeners.py
+++ botutils/packages/listeners.py
@@ -27,7 +27,7 @@ class CreateListener:
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_reaction(self, check, timeout=60, ignore_timeout=True):
+    async def get_reaction(self, check, timeout=60, ignore_timeout=True, duel=False):
         target_id = parse_check(check)  # type: int
 
         def predicate(r, u):
@@ -37,6 +37,32 @@ class CreateListener:
             predicate = target_id
 
         coro = self.bot.wait_for("reaction_add", check=predicate, timeout=timeout)
+
+        if duel:
+            async def wait_for(event):
+                coro = self.bot.wait_for(event, check=predicate, timeout=timeout)
+                try:
+                    return await coro
+                except:
+                    return None
+
+            tasks = [
+                self.bot.loop.create_task(wait_for("reaction_add")),
+                self.bot.loop.create_task(wait_for("reaction_remove"))
+            ]
+            while True:
+                for i, task in enumerate(tasks):
+                    await asyncio.sleep(0)
+                    if task.done() and task.exception():
+                        raise self.bot.ignored_exit
+                    if task.done():
+                        tasks[0 if i == 1 else 1].cancel()
+                        result = task.result()
+                        if result is None:
+                            raise self.bot.ignored_exit
+                        return result
+                await asyncio.sleep(0.01)
+
         if ignore_timeout:
             try:
                 reaction, user = await coro
