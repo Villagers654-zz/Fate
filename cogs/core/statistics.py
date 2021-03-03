93091765a402d1f1d96232b19c5ba2f864db2f56
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-03-03 02:23:07 -0500

    switch to using getattr() instead of using eval

--- cogs/core/statistics.py
+++ cogs/core/statistics.py
@@ -22,12 +22,10 @@ class Statistics(commands.Cog):
         e = discord.Embed(color=colors.fate())
         owner = await self.bot.fetch_user(self.bot.config["bot_owner_id"])
         e.set_author(name="Module Statistics", icon_url=owner.avatar_url)
-        e.description = self.bot.utils.format_dict(
-            {
-                key: f"{len(eval(f'self.bot.get_cog(key).{value}', dict(self=self, key=key)))} active"
-                for key, value in self.cogs.items()
-            }
-        )
+        e.description = self.bot.utils.format_dict({
+            key: f"{len(getattr(self.bot.cogs[key], value))} active"
+            for key, value in self.cogs.items()
+        })
         await ctx.send(embed=e)
 
 
