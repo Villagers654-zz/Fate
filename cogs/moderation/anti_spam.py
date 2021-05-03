03cdfb657c609ac7eb2efd47df0f3299548671b9
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-05-03 12:59:54 -0400

    fix the inhuman module triggering against long links

--- cogs/moderation/anti_spam.py
+++ cogs/moderation/anti_spam.py
@@ -633,33 +633,35 @@ class AntiSpam(commands.Cog):
                 # non abc char spam
                 if conf["non_abc"]:
                     if len(msg.content) > 256 and not has_abcs:
-                        reason = "non abc"
+                        reason = "Inhuman: non abc"
                         triggered = True
 
                 # Tall msg spam
                 if conf["tall_messages"]:
                     if len(content.split("\n")) > 8 and sum(len(line) for line in lines if line) < 21:
-                        reason = "tall message"
+                        reason = "Inhuman: tall message"
                         triggered = True
                     elif len(content.split("\n")) > 5 and not has_abcs:
-                        reason = "tall message"
+                        reason = "Inhuman: tall message"
                         triggered = True
 
                 # Empty lines spam
                 if conf["empty_lines"]:
                     if len([l for l in lines if not l]) > len([l for l in lines if l]) and len(lines) > 8:
-                        reason = "too many empty lines"
+                        reason = "Inhuman: too many empty lines"
                         triggered = True
 
                 # Mostly unknown chars spam
                 if conf["unknown_chars"]:
                     if len(content) > 128 and len(content) / total_abcs > 3:
-                        triggered = True
+                        if not ("http" in content and len(content) < 512):
+                            reason = "Inhuman: mostly non abc characters"
+                            triggered = True
 
                 # ASCII / Spammed chars
                 if conf["ascii"]:
                     if len(content) > 256 and len(content) / total_spaces > 10:
-                        reason = "ascii"
+                        reason = "Inhuman: ascii"
                         triggered = True
 
                 # Pasting large messages without typing much, or at all
