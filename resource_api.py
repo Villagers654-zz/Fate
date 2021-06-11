7aca43d2637a45a706994f993b1866e3ebc27a37
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-06-11 02:47:18 -0400

    make resource api class oriented

--- resource_api.py
+++ resource_api.py
@@ -8,61 +8,56 @@ import os
 import aiofiles
 
 
-cd = {}
+class ResourcesAPI:
+    def __init__(self, port=80):
+        self.port = port
+        self.cd = {}
+
+        self.app = web.Application()
+        self.app.router.add_get("/{tail:.*}", self.get_resource)
+        self.app.router.add_get('/reactions/{tail:.*/[a-zA-Z0-9]*\\....}', self.get_resource)
+
+    def run(self):
+        web.run_app(self.app, port=80)
+
+    def is_on_cooldown(self, ip) -> bool:
+        now = int(time() / 25)
+        if ip not in self.cd:
+            self.cd[ip] = [now, 0]
+        if self.cd[ip][0] == now:
+            self.cd[ip][1] += 1
+        else:
+            self.cd[ip] = [now, 0]
+        if self.cd[ip][1] > 3:
+            return True
+        return False
+
+    async def get_resource(self, request):
+        """Handler for streaming reaction GIFs"""
+        if self.is_on_cooldown(request.remote):
+            return web.Response(text="You are being rate-limited", status=404)
+
+        # Ensure the file exists
+        path = request.path
+        if path.startswith("/reactions"):
+            fp = f"./data/images/reactions/{path.lstrip('/reactions')}"
+        else:
+            fp = f"./assets/{path.lstrip('/assets')}"
+        if os.path.isdir(fp) or not os.path.exists(fp):
+            return web.HTTPNotFound()
+        print(f"Received a request for {request.path}")
+
+        # Stream the gif
+        async with aiofiles.open(fp, "rb") as f:
+            file = await f.read()
+        resp = web.StreamResponse()
+        ext = fp[-(len(fp) - fp.rfind(".")):].lstrip(".")
+        resp.headers["Content-Type"] = f"Image/{ext.upper()}"
+        resp.headers["Content-Disposition"] = f"filename='file.{ext}';"
+        await resp.prepare(request)
+        await resp.write(file)
+        await resp.write_eof()
 
 
-async def check_rate_limit(request):
-    ip = request.remote
-    now = int(time() / 25)
-    if ip not in cd:
-        cd[ip] = [now, 0]
-    if cd[ip][0] == now:
-        cd[ip][1] += 1
-    else:
-        cd[ip] = [now, 0]
-    if cd[ip][1] > 2:
-        return True
-    return False
-
-
-async def get_resource(request):
-    """Handler for streaming reaction GIFs"""
-    ip = request.remote
-    now = int(time() / 25)
-    if ip not in cd:
-        cd[ip] = [now, 0]
-    if cd[ip][0] == now:
-        cd[ip][1] += 1
-    else:
-        cd[ip] = [now, 0]
-    if cd[ip][1] > 3:
-        return web.Response(text="You are being rate-limited", status=404)
-
-    # Ensure the file exists
-    path = request.path
-    if path.startswith("/reactions"):
-        fp = f"./data/images/reactions/{path.lstrip('/reactions')}"
-    else:
-        fp = f"./assets/{path.lstrip('/assets')}"
-    if not os.path.exists(fp):
-        return web.HTTPNotFound()
-    print(f"Received a request for {request.path}")
-
-    # Stream the gif
-    async with aiofiles.open(fp, "rb") as f:
-        file = await f.read()
-    resp = web.StreamResponse()
-    ext = fp[-(len(fp) - fp.rfind(".")):].lstrip(".")
-    resp.headers["Content-Type"] = f"Image/{ext.upper()}"
-    resp.headers["Content-Disposition"] = f"filename='file.{ext}';"
-    await resp.prepare(request)
-    await resp.write(file)
-    await resp.write_eof()
-
-
-app = web.Application()
-app.router.add_get("/{tail:.*}", get_resource)
-app.router.add_get('/reactions/{tail:.*/[a-zA-Z0-9]*\\....}', get_resource)
-
-
-web.run_app(app, port=80)
+app = ResourcesAPI()
+app.run()
