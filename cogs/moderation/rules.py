7227205b33f9135248a42d4d6502ffa4de91f90a
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-06-28 21:46:28 -0400

    refactor: use MongoDB instead of JSON for rules module

--- cogs/moderation/rules.py
+++ cogs/moderation/rules.py
@@ -1,51 +1,46 @@
+"""
+cogs.moderation.rules
+~~~~~~~~~~~~~~~~~~~~~~
+
+A module for adding in a rules command to display the servers rules quickly
+
+:copyright: (C) 2019-present Michael Stollings
+:license: Proprietary and Confidential, see LICENSE for details
+"""
+
 from discord.ext import commands
-from os.path import isfile
 import discord
-import json
 
 
 class Mod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.rules = {}
-        if isfile("./data/userdata/rules.json"):
-            with open("./data/userdata/rules.json", "r") as infile:
-                dat = json.load(infile)
-                if "rules" in dat:
-                    self.rules = dat["rules"]
-
-    def save_data(self):
-        with open("./data/userdata/rules.json", "w") as outfile:
-            json.dump({"rules": self.rules}, outfile, ensure_ascii=False)
+        self.rules = bot.utils.cache("rules")
 
     @commands.group(name="rules")
-    async def _rules(self, ctx):
-        if ctx.invoked_subcommand is None:
-            if str(ctx.guild.id) not in self.rules:
-                await ctx.send(
-                    "This server doesnt have any rules set, try using .rules help"
-                )
-            else:
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def rules(self, ctx):
+        if not ctx.invoked_subcommand:
+            if ctx.guild.id in self.rules:
                 e = discord.Embed(color=0xFF0000)
-                e.description = self.rules[str(ctx.guild.id)]
-                await ctx.send(embed=e)
+                e.description = self.rules[ctx.guild.id]["content"]
+                return await ctx.send(embed=e)
+            await ctx.send(
+                "This server doesnt have any rules set, try using .rules help"
+            )
 
-    @_rules.command(name="help")
+    @rules.command(name="help")
     async def _help(self, ctx):
         await ctx.send("**Rules Usage:**\n" ".rules set {rules}")
 
-    @_rules.command(name="set")
+    @rules.command(name="set")
     @commands.has_permissions(manage_guild=True)
-    async def _set(self, ctx, *, rules):
-        self.rules[str(ctx.guild.id)] = rules
+    async def _set(self, ctx, *, new_rules):
+        self.rules[ctx.guild.id] = {
+            "content": new_rules
+        }
         await ctx.send("Successfully set the rules 👍")
-        self.save_data()
-
-    @commands.Cog.listener()
-    async def on_guild_remove(self, guild):
-        guild_id = str(guild.id)
-        if guild_id in self.rules:
-            del self.rules[guild_id]
+        await self.rules.flush()
 
 
 def setup(bot):
