b47746d8fd9529f148377809dfade13c0a3dd17e
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-07-09 18:06:27 -0400

    refactor: add support for select menus in the future

--- cogs/utility/buttonroles.py
+++ cogs/utility/buttonroles.py
@@ -25,6 +25,8 @@ class ButtonRoles(commands.Cog):
     def __init__(self, bot: Fate):
         self.bot = bot
         self.menus = bot.utils.cache("button_roles")
+        for key in self.menus.keys():
+            self.menus.remove(key)
         self.global_cooldown = bot.utils.cooldown_manager(1, 3)
         if bot.is_ready():
             bot.loop.create_task(self.load_menus_on_start())
@@ -34,9 +36,39 @@ class ButtonRoles(commands.Cog):
         if not hasattr(self.bot, "menus_loaded"):
             self.bot.menus_loaded = False
         if not self.bot.menus_loaded:
-            for msg_id in self.menus.keys():
-                self.bot.add_view(Menu(self, msg_id))
-            self.bot.menus_loaded = True
+            for guild_id, menus in self.menus.items():
+                for msg_id, data in menus.items():
+                    if data["style"] == "buttons":
+                        self.bot.add_view(ButtonMenu(self, guild_id, msg_id))
+                self.bot.menus_loaded = True
+
+    @commands.group(name="role-menu", aliases=["rolemenu"])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    @commands.guild_only()
+    @commands.bot_has_permissions(embed_links=True)
+    async def role_menu(self, ctx: commands.Context):
+        if not ctx.invoked_subcommand:
+            e = discord.Embed(color=self.bot.config["theme_color"])
+            e.set_author(name="Role Menus", icon_url=self.bot.user.avatar.url)
+            if ctx.guild.icon:
+                e.set_thumbnail(url=ctx.guild.icon.url)
+            e.description = "Create menus for users to self assign roles via buttons"
+            p: str = ctx.prefix
+            e.add_field(
+                name="◈ Usage",
+                value=f"{p}role-menu create\n"
+                      f"{p}role-menu set-message `msg_id` `new message`\n"
+                      f"{p}role-menu add-role `msg_id` `@role`\n"
+                      f"{p}role-menu remove-role `msg_id` `@role`\n"
+                      f"{p}role-menu set-style `msg_id` `button/dropdown`"
+            )
+            count = 0
+            if ctx.guild.id in self.menus:
+                count = len(self.menus[ctx.guild.id])
+            e.set_footer(text=f"{count} Active Menu")
+            if count == 0 or count > 1:
+                e.footer.text += "s"
+            await ctx.send(embed=e)
 
     @commands.command(name="test")
     @commands.is_owner()
@@ -56,32 +88,36 @@ class ButtonRoles(commands.Cog):
         reply = await convo.ask("#Mention the channel you want me to use")
         channel = reply.channel_mentions[0]
         msg = await channel.send("Choose your role")
-        self.menus[msg.id] = {
-            "guild_id": ctx.guild.id,
-            "roles": data
+        if ctx.guild.id not in self.menus:
+            self.menus[ctx.guild.id] = {}
+        self.menus[ctx.guild.id][str(msg.id)] = {
+            "roles": data,
+            "text": "Select your role",
+            "style": "buttons",
         }
-        view = Menu(cls=self, message_id=msg.id)
+        view = ButtonMenu(cls=self, guild_id=ctx.guild.id, message_id=msg.id)
         await msg.edit(view=view)
         await self.menus.flush()
 
     @commands.Cog.listener()
-    async def on_message_delete(self, message):
-        if message.id in self.menus:
-            await self.menus.remove(message.id)
+    async def on_raw_message_delete(self, payload):
+        if payload.guild_id in self.menus:
+            if str(payload.message_id) in self.menus[payload.guild_id]:
+                await self.menus.remove_sub(payload.guild_id, payload.message_id)
 
 
-class Menu(ui.View):
-    def __init__(self, cls: ButtonRoles, message_id: int):
+class ButtonMenu(ui.View):
+    def __init__(self, cls: ButtonRoles, guild_id: int, message_id: int):
         self.bot = cls.bot
         self.menus = cls.menus
         self.global_cooldown = cls.global_cooldown
         self.cooldown = cls.bot.utils.cooldown_manager(5, 25)
-        self.message_id = message_id
+        self.guild_id = guild_id
         self.buttons = {}
         super().__init__(timeout=None)
 
-        data = self.menus[message_id]
-        guild = self.bot.get_guild(data["guild_id"])
+        data = self.menus[guild_id][str(message_id)]
+        guild = self.bot.get_guild(guild_id)
         for role_id in data["roles"]:
             role = guild.get_role(role_id)
             if not role:
@@ -107,7 +143,7 @@ class Menu(ui.View):
             """ Remove a button that can no longer be used """
             self.remove_item(self.buttons[custom_id])
             with suppress(KeyError):
-                self.menus[key]["roles"].remove(role_id)
+                self.menus[self.guild_id][key]["roles"].remove(role_id)
             await self.menus.flush()
             await interaction.message.edit(view=self)
             return await interaction.response.send_message(reason, ephemeral=True)
@@ -122,12 +158,11 @@ class Menu(ui.View):
 
         # Parse the key and get its relative data
         custom_id = interaction.data["custom_id"]
-        key = int(custom_id.split("@")[1])
+        key = custom_id.split("@")[1]
         role_id = int(custom_id.split("@")[0])
-        data: dict = self.menus[key]
 
         # Fetch required variables
-        guild = self.bot.get_guild(data["guild_id"])
+        guild = self.bot.get_guild(self.guild_id)
         member = guild.get_member(interaction.user.id)
         role = guild.get_role(role_id)
         name = self.buttons[custom_id].label
