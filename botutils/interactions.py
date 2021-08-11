893309361589decb34dde6548c7aa85f4eae4437
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-08-11 03:00:02 -0400

    feat: add template views

---
+++ botutils/interactions.py
@@ -0,0 +1,49 @@
+"""
+botutils.interactions
+~~~~~~~~~~~~~~~~~~~~~~
+
+A module for template interaction classes
+
+Classes:
+    ModView : A View that only moderators can interact with
+    AuthorView : A view that only the author of the original message can interact with
+
+:copyright: (C) 2020-present Michael Stollings
+:license: Proprietary and Confidential, see LICENSE for details
+"""
+
+from discord import ui, Interaction
+from discord.ext.commands import Context
+from . import CooldownManager
+
+
+class ModView(ui.View):
+    ctx: Context
+    cd: CooldownManager
+
+    async def interaction_check(self, interaction: Interaction):
+        """ Ensure the interaction is from the user who initiated the view """
+        member = interaction.guild.get_member(interaction.user.id)
+        if not self.ctx.bot.attrs.is_moderator(member):
+            await interaction.response.send_message(
+                "Only moderators can interact", ephemeral=True
+            )
+            return False
+        if self.cd.check(member.id):
+            return False
+        return True
+
+
+class AuthorView(ui.View):
+    ctx: Context
+    cd: CooldownManager
+
+    async def interaction_check(self, interaction: Interaction):
+        if interaction.user.id != self.ctx.author.id:
+            await interaction.response.send_message(
+                "Only moderators can interact", ephemeral=True
+            )
+            return False
+        if self.cd.check(interaction.user.id):
+            return False
+        return True
