68812fa775c8fd8ba0fb9e93aa269cd2d20e1c98
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-09-19 19:25:30 -0400

    feat: document exceptions.py

--- classes/exceptions.py
+++ classes/exceptions.py
@@ -12,10 +12,15 @@ from aiohttp import web
 
 
 class EmptyException(Exception):
+    """
+    An exception ignored by the bots error handler.
+    This is used to easily stop operation inside something nested
+    """
     pass
 
 
 class aiohttp:
+    """ Web response shortcuts """
     rate_limit = web.Response(text="You are being rate-limited", status=404)
     invalid_data = web.Response(text="Invalid data", status=404)
     invalid_login = web.Response(text="Invalid Login Credentials", status=404)
