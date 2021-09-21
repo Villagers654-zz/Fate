61f595fb6f562fec009bb0bf0f4e838a542eaa16
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-09-21 02:50:57 -0400

    feat: add lockdown feature to the lock command

--- cogs/moderation/lock.py
+++ cogs/moderation/lock.py
@@ -21,6 +21,7 @@ from fate import Fate
 
 
 locks = {
+    "lockdown": "Prevents everyone from talking",
     "kick": "Kicks all new members",
     "ban": "Bans all new members",
     "mute": "Mutes all new members",
@@ -44,12 +45,15 @@ class Lock(commands.Cog):
             if ctx.guild.id not in self.lock:
                 self.lock[ctx.guild.id] = {}
 
+    async def cog_after_invoke(self, ctx):
+        if ctx.guild.id in self.lock and not self.lock[ctx.guild.id]:
+            self.lock.remove(ctx.guild.id)
+
     @commands.command(name="lock", description="Kicks/bans/mutes users that join the server")
     @commands.has_permissions(administrator=True)
     async def lock(self, ctx):
-        choices = [": ".join(item) for item in locks.items()]
-        choice = await GetChoice(ctx, choices)
-        lock = list(locks.keys())[choices.index(choice)]
+        choice = await GetChoice(ctx, dict(locks))
+        lock = list(locks.keys())[list(locks.keys()).index(choice)]
         guild_id = ctx.guild.id
         if guild_id in self.lock:
             conflict = [ltype for ltype in self.lock[guild_id].keys() if ltype in unique]
@@ -89,7 +93,8 @@ class Lock(commands.Cog):
                 return await ctx.send("Invalid format")
             self.lock[guild_id][lock] = {"age_lmt": min_age}
 
-        await ctx.send(f"Locked the server")
+        if lock != "lockdown":
+            await ctx.send(f"Locked the server")
         await self.lock.flush()
 
         # Check members that have already joined
@@ -119,12 +124,79 @@ class Lock(commands.Cog):
                             await member.kick(reason=f"Didn't pass minimum age requirement set by {ctx.author}")
                     await ctx.send(f"Successfully kicked {len(violations)} accounts")
 
+        if lock == "lockdown":
+            # Choose between locking the channel or the whole server
+            choice = await GetChoice(ctx, ["Lock this channel", "Lock the whole server"])
+            channels = ctx.guild.text_channels
+            if choice == "Lock this channel":
+                channels = [ctx.channel]
+
+            # Save the old permissions and edit the new ones in
+            self.lock[guild_id][lock] = {}
+            for channel in channels:
+                if channel.permissions_for(ctx.guild.me).manage_channels:
+                    original_perms = {
+                        str(obj.id): list(perms) for obj, perms in channel.overwrites.items()
+                    }
+                    locked_perms = channel.overwrites
+                    for overwrite in locked_perms.keys():
+                        locked_perms[overwrite].send_messages = False
+                    if ctx.guild.default_role not in locked_perms:
+                        new = discord.PermissionOverwrite()
+                        new.update(send_messages=False)
+                        locked_perms[ctx.guild.default_role] = new
+                    try:
+                        await channel.edit(overwrites=locked_perms)
+                        self.lock[guild_id][lock][str(channel.id)] = original_perms
+                    except discord.errors.Forbidden:
+                        pass
+
+            # Check if no channels were edited
+            if not self.lock[guild_id][lock]:
+                self.lock.remove_sub(guild_id, lock)
+                return await ctx.send("Seems I wasn't able to modify any channels")
+
+            s = "s" if len(self.lock[guild_id][lock]) > 1 else ""
+            await ctx.send(f"Locked down {len(self.lock[guild_id][lock])} channel{s}")
+            await self.lock.flush()
+
     @commands.command(name="unlock", description="Removes any active locks")
     @commands.has_permissions(administrator=True)
     async def unlock(self, ctx):
         guild_id = ctx.guild.id
         if guild_id not in self.lock:
             return await ctx.send("There currently isn't active lock")
+
+        if "lockdown" in self.lock[guild_id]:
+            total_unlocked = 0
+
+            for channel_id, overwrites in self.lock[guild_id]["lockdown"].items():
+                channel: discord.TextChannel = self.bot.get_channel(int(channel_id))  # type: ignore
+                if not channel:
+                    continue
+                reconstructed = {}
+                for object_id, perms in overwrites.items():
+                    object_id = int(object_id)
+                    object = ctx.guild.get_role(object_id) or ctx.guild.get_member(object_id)
+                    if not object:
+                        continue
+                    permissions = discord.PermissionOverwrite(**{
+                        perm: value for perm, value in perms
+                    })
+                    reconstructed[object] = permissions
+                try:
+                    await channel.edit(overwrites=reconstructed)
+                    total_unlocked += 1
+                except discord.errors.Forbidden:
+                    pass
+
+            self.lock.remove_sub(guild_id, "lockdown")
+            if not total_unlocked:
+                return await ctx.send("I wasn't able to manage any of the locked channels")
+
+            s = "s" if total_unlocked > 1 else ""
+            return await ctx.send(f"Unlocked {total_unlocked} channel{s}")
+
         has_bans = False
         if "ban" in self.lock[guild_id] or "new" in self.lock[guild_id]:
             has_bans = True
