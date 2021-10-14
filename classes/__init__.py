d1de7b91b1d3ce076004f964312acad64c06ac33
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-10-14 05:57:46 -0400

    feat: change the module docstring for classes

--- classes/__init__.py
+++ classes/__init__.py
@@ -2,14 +2,14 @@
 Core classes wrapper
 ~~~~~~~~~~~~~~~~~~~~~
 
-An ease of use wrapper for use with Fate(written in discord.py)
+A general classes wrapper for use with Fate(written in discord.py)
 Contains checks, and exception classes
 
 :copyright: (C) 2021-present FrequencyX4, All Rights Reserved
 :license: Proprietary, see LICENSE for details
 """
 
-__title__ = "BotClasses"
+__title__ = "General Classes"
 __author__ = "FrequencyX4"
 __license__ = "Proprietary, see LICENSE for details"
 __copyright__ = "Copyright (C) 2021-present FrequencyX4, All Rights Reserved"
