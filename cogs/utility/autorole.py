519f8cc37543f97f0b052286590e5c11fab0d189
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-10-15 14:31:30 -0400

    refactor: convert autorole from json to mongodb

--- cogs/utility/autorole.py
+++ cogs/utility/autorole.py
@@ -2,47 +2,41 @@
 cogs.utility.autorole
 ~~~~~~~~~~~~~~~~~~~~~~
 
-Less api spammy invite manager that relies on high uptime
+A module for granting users role(s) when they join
 
 :copyright: (C) 2021-present FrequencyX4, All Rights Reserved
 :license: Proprietary, see LICENSE for details
 """
 
 from discord.ext import commands
-from os.path import isfile
-from contextlib import suppress
+from typing import *
 from botutils import colors
 import discord
 from discord.errors import NotFound, Forbidden
-import json
 
 
 class AutoRole(commands.Cog):
+    default_config = {
+        "wait_for_verify": False,
+        "roles": [],
+    }
+
     def __init__(self, bot):
         self.bot = bot
-        self.roles = {}
-        if isfile("./data/userdata/autorole.json"):
-            with open("./data/userdata/autorole.json", "r") as infile:
-                dat = json.load(infile)
-                if "roles" in dat:
-                    self.roles = dat["roles"]
-
-    async def save_data(self):
-        await self.bot.utils.save_json("./data/userdata/autorole.json", {"roles": self.roles})
+        self.config = bot.utils.cache("autorole")
 
     def is_enabled(self, guild_id):
-        return str(guild_id) in self.roles
+        return guild_id in self.config
 
     @commands.command(
         name="autorole", description="Adds a role to users when they join"
     )
     @commands.has_permissions(manage_roles=True)
     @commands.bot_has_permissions(embed_links=True, manage_roles=True)
-    async def _autorole(self, ctx, item: commands.clean_content = None):
-        item = item  # type: str
-        guild_id = str(ctx.guild.id)
+    async def _autorole(self, ctx, *, args: Union[discord.Role, str] = None):
+        guild_id = ctx.guild.id
         e = discord.Embed(color=colors.fate)
-        if item is None:
+        if not args:
             e.set_author(name="Auto-Role Help", icon_url=self.bot.user.display_avatar.url)
             e.set_thumbnail(url=ctx.author.display_avatar.url)
             e.add_field(
@@ -51,113 +45,105 @@ class AutoRole(commands.Cog):
                 inline=False,
             )
             return await ctx.send(embed=e)
-        if item.lower() == "clear":
-            if guild_id not in self.roles:
+
+        if args == "clear":
+            if guild_id not in self.config:
                 return await ctx.send("Auto role is not active")
-            del self.roles[guild_id]
-            await self.save_data()
+            self.config[guild_id]["roles"] = []
+            if self.config[guild_id] == self.default_config:
+                await self.config.remove(guild_id)
+            else:
+                await self.config.flush()
             return await ctx.send("Cleared list of roles")
-        if item.lower() == "list":
-            if guild_id not in self.roles:
+
+        if args == "list":
+            if guild_id not in self.config:
                 return await ctx.send("Auto role is not active")
             e.set_author(name="Auto Roles", icon_url=self.bot.user.display_avatar.url)
             e.description = ""
-            for role_id in self.roles[guild_id]:
+            for role_id in self.config[guild_id]["roles"]:
                 role = ctx.guild.get_role(role_id)
                 if not role:
-                    self.roles[guild_id].remove(role_id)
-                    await self.save_data()
+                    self.config[guild_id]["roles"].remove(role_id)
+                    await self.config.flush()
                     continue
                 e.description += f"• {role.name}\n"
             return await ctx.send(embed=e)
-        item = item.replace("@", "").lower()
-        if guild_id not in self.roles:
-            self.roles[guild_id] = []
-        for role in ctx.message.role_mentions:
-            if ctx.author.id != ctx.guild.owner.id:
-                if role.position > ctx.author.top_role.position:
-                    return await ctx.send("That roles above your paygrade, take a seat")
-                if role.id in self.roles[guild_id]:
-                    return await ctx.send("That roles already in use")
-            self.roles[guild_id].append(role.id)
-            await self.save_data()
-            return await ctx.send(f"Added `{role.name}` to the list of auto roles")
-        for role in ctx.guild.roles:
-            if item == role.name.lower():
-                if role.position > ctx.author.top_role.position:
-                    return await ctx.send("That roles above your paygrade, take a seat")
-                if role.id in self.roles[guild_id]:
-                    return await ctx.send("That roles already in use")
-                self.roles[guild_id].append(role.id)
-                await self.save_data()
-                return await ctx.send(f"Added `{role.name}` to the list of auto roles")
-        for role in ctx.guild.roles:
-            if item in role.name.lower():
-                if role.position > ctx.author.top_role.position:
-                    return await ctx.send("That roles above your paygrade, take a seat")
-                if role.id in self.roles[guild_id]:
-                    return await ctx.send("That roles already in use")
-                self.roles[guild_id].append(role.id)
-                await self.save_data()
-                return await ctx.send(f"Added `{role.name}` to the list of auto roles")
-        await ctx.send("Role not found")
+
+        if guild_id not in self.config:
+            self.config[guild_id] = self.default_config
+
+        # Convert the args into a Role
+        if isinstance(args, discord.Role):
+            role = args
+        else:
+            role = await self.bot.utils.get_role(ctx, args)
+            if not role:
+                return await ctx.send("Role not found")
+
+        # Check the author, and roles position
+        if ctx.author.id != ctx.guild.owner.id:
+            if role.position >= ctx.author.top_role.position:
+                return await ctx.send("That role's above your paygrade, take a seat")
+            if role.id in self.config[guild_id]["roles"]:
+                return await ctx.send("That role's already in use")
+
+        self.config[guild_id]["roles"].append(role.id)
+        await self.config.flush()
+        await ctx.send(f"Added `{role.name}` to the list of auto roles")
 
     @commands.Cog.listener()
-    async def on_member_join(self, m: discord.Member):
-        guild_id = str(m.guild.id)
-        if guild_id in self.roles:
-            if not m.guild.me.guild_permissions.manage_roles:
+    async def on_member_join(self, member: discord.Member):
+        guild_id = member.guild.id
+        if guild_id in self.config:
+            guild = member.guild
+            bot = guild.me
+            if not bot.guild_permissions.manage_roles:
                 try:
-                    dm = m.guild.owner.dm_channel
+                    dm = guild.owner.dm_channel
                     if not dm:
-                        dm = await m.guild.owner.create_dm()
+                        dm = await guild.owner.create_dm()
                     history = await dm.history(limit=1).flatten()
                     if history and "AutoRole" in history[0].content:
                         return
-                except (Forbidden, NotFound, AttributeError):
-                    return
-                try:
-                    await m.guild.owner.send(
-                        f"**[AutoRole - {m.guild.name}] I'm missing manage_roles permissions "
+                    await guild.owner.send(
+                        f"**[AutoRole - {guild}] I'm missing manage_roles permissions "
                         f"in order to add roles to new users"
                     )
                 except (Forbidden, NotFound, AttributeError):
-                    pass
+                    return
                 return
-            for role_id in self.roles[guild_id]:
-                role = m.guild.get_role(role_id)
+            for role_id in self.config[guild_id]["roles"]:
+                role = guild.get_role(role_id)
                 if not role:
-                    self.roles[guild_id].remove(role_id)
-                    await self.save_data()
+                    self.config[guild_id]["roles"].remove(role_id)
+                    await self.config.flush()
                     continue
-                if role.position >= m.guild.me.top_role.position:
+                if role.position >= bot.top_role.position:
                     try:
-                        await m.guild.owner.send(
-                            f"**[AutoRole - {m.guild.name}] can't add {role.name} to user. "
+                        await guild.owner.send(
+                            f"**[AutoRole - {bot.guild.name}] can't add {role.name} to user. "
                             f"Its position is higher than mine. You'll need to re-add it"
                         )
                     except discord.errors.Forbidden:
                         pass
-                    self.roles[guild_id].remove(role_id)
+                    self.config[guild_id]["roles"].remove(role_id)
+                    await self.config.flush()
                 else:
                     try:
-                        await m.add_roles(role)
-                    except discord.errors.NotFound:
+                        await member.add_roles(role)
+                    except discord.errors.Forbidden:
+                        self.config[guild_id]["roles"].remove(role_id)
+                        await self.config.flush()
+                    except (discord.errors.NotFound, discord.errors.HTTPException):
                         pass
 
     @commands.Cog.listener()
     async def on_role_delete(self, role):
-        guild_id = str(role.guild.id)
-        if role.id in self.roles[guild_id]:
-            self.roles[guild_id].pop(self.roles[guild_id].index(role.id))
-            await self.save_data()
-
-    @commands.Cog.listener()
-    async def on_guild_remove(self, guild):
-        guild_id = str(guild.id)
-        if guild_id in self.roles:
-            del self.roles[guild_id]
-            await self.save_data()
+        guild_id = role.guild.id
+        if role.id in self.config[guild_id]["roles"]:
+            self.config[guild_id]["roles"].remove(role.id)
+            await self.config.flush()
 
 
 def setup(bot):
