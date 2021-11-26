d30238e879a67901884a7c240037870922ff3bd4
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2021-11-26 06:27:45 -0500

    feat: final touches on cache rewrite

--- botutils/cache_rewrite.py
+++ botutils/cache_rewrite.py
@@ -23,7 +23,7 @@ class Cache:
     instances: Dict[Any, "Data"] = {}
     changes = {}
 
-    def __init__(self, bot, collection: str) -> None:
+    def __init__(self, bot, collection: str):
         self.bot = bot
         self.collection = collection
 
@@ -31,7 +31,7 @@ class Cache:
     def _db(self) -> AsyncIOMotorCollection:
         return self.bot.aio_mongo[self.collection]
 
-    async def count(self):
+    async def count(self) -> int:
         return await self._db.estimated_document_count()
 
     def __getitem__(self, key) -> "Cache.get":
@@ -54,18 +54,18 @@ class Cache:
             _id = document.pop("_id")
             yield _id, document
 
-    async def _get(self, key) -> Optional[dict]:
+    async def _get(self, key) -> dict:
         query = await self._db.find({"_id": key}).to_list(length=1)
         for result in query:
             result.pop("_id")
             return result
-        return None
+        return {}
 
-    async def get(self, key) -> Optional["Data"]:
+    async def get(self, key) -> "Data":
         # Remove unused instances
         for key, instance in list(self.instances.items()):
             await asyncio.sleep(0)
-            if time() - 300 > instance.last_sync:
+            if time() - 300 > instance.last_update:
                 if key in self.instances:
                     del self.instances[key]
 
@@ -74,8 +74,6 @@ class Cache:
             return await self.instances[key].reinstate()
 
         result = await self._get(key)
-        if not result:
-            return None
 
         # Double check after handing off the loop
         if key in self.instances:
@@ -84,7 +82,7 @@ class Cache:
         self.instances[key] = Data(self, key, result)
         return self.instances[key]
 
-    async def flush(self) -> None:
+    async def flush(self):
         """ Pushes changes from cache into the actual database """
         collection = self.bot.aio_mongo[self.collection]
         for key, value in list(self.changes.items()):
@@ -99,43 +97,90 @@ class Cache:
 
     async def remove(self, key):
         """ An awaitable to remove an item from the cache, and database """
-        await self._db.delete_one({key: 1})
+        await self._db.delete_one({"_id": key})
 
 
 class Data(dict):
+    """ Represents a temporary dataclass-like object """
     def __init__(self, state: Cache, key, data):
-        self.state = state
+        self._state = state
         self.key = key
 
         for _key, value in data.items():
+            if value.__class__.__name__ == "dict":
+                value = self.make_subclass(deepcopy(value))
             self[_key] = value
         self.copy = deepcopy(data)
 
-        self.last_sync = time()
+        self.last_update = time()
         super().__init__()
+
+    def __setitem__(self, key, value):
+        """ Make sure nested dictionaries inherit from NestedData """
+        if value.__class__.__name__ == "dict":
+            value = self.make_subclass(deepcopy(value))
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        self.last_update = time()
+        super().__delitem__(key)
+
+    def make_subclass(self, dictionary: dict) -> "NestedData":
+        """ Make sure nested dictionaries inherit from NestedData """
+        for key, value in list(dictionary.items()):
+            if value.__class__.__name__ == "dict":
+                dictionary[key] = self.make_subclass(value)
+        return NestedData(self, dictionary)
 
     async def reinstate(self) -> "Data":
         """ Ran when another process starts using the same object """
-        if time() - 300 > self.last_sync:
+        if time() - 300 > self.last_update:
             await self.sync()
         return self
 
     async def sync(self):
         """ Merge with changes to the DB variant """
-        new_data = await self.state._get(self.key)
+        new_data = await self._state._get(self.key)  # type: ignore
         for key, value in new_data.items():
             await asyncio.sleep(0)
             if key not in self.copy or value != self.copy[key]:
                 self[key] = value
-        self.last_sync = time()
+        self.last_update = time()
 
     async def flush(self, manual: bool = True):
+        if self.copy and not self.keys():
+            self.copy = {}
+            return await self._state.remove(self.key)
         if manual or dict(self) != self.copy:
-            self.state.changes[self.key] = dict(self)
+            self._state.changes[self.key] = dict(self)
         else:
+            # Check nested values
             for key, value in self.items():
                 await asyncio.sleep(0)
                 if value != self.copy[key]:
-                    self.state.changes[self.key] = dict(self)
+                    self._state.changes[self.key] = dict(self)
                     break
-        await self.state.flush()
+
+        await self._state.flush()
+        db_state = await self._state._get(self.key)
+        self.copy = db_state or {}
+
+
+class NestedData(dict):
+    """ Updates the parent dictionaries last_update variable """
+    def __init__(self, parent: "Data", data: dict):
+        self._parent = parent
+        super().__init__(**data)
+
+    def __setitem__(self, key, value):
+        # Subclass any new dictionaries
+        if value.__class__.__name__ == "dict":
+            value = self._parent.make_subclass(value)
+
+        self._parent.last_update = time()
+
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        self._parent.last_update = time()
+        super().__delitem__(key)
